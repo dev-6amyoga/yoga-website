6c7c333cce765e8f8757cf08aa324ba9f0fb3a1e
smriti0302 <smriti030202@gmail.com>
2023-12-11 22:51:10 +0530

    plans

--- backend/routes/Plan.js
+++ backend/routes/Plan.js
@@ -19,4 +19,20 @@ router.get("/get-all-plans", async (req, res) => {
   }
 });
 
+router.get("/get-all-student-plans", async (req, res) => {
+  try {
+    const plans = await Plan.findAll({
+      where: {
+        plan_user_type: "student",
+      },
+    });
+    return res.status(HTTP_OK).json({ plans });
+  } catch (error) {
+    console.error("Error fetching plans:", error);
+    return res
+      .status(HTTP_INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
