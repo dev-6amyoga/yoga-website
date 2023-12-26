46051f0ad8713c50b388cb63754a08c07895ca3c
smriti0302 <smriti030202@gmail.com>
2023-12-26 11:06:29 +0530

    teacher barebones

--- frontend/src/store/UserStore.js
+++ frontend/src/store/UserStore.js
@@ -1,37 +1,37 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useUserStore = create((set) => ({
-    user: null,
-    setUser: (user) =>
-        set(() => {
-            // console.log('updating user : ', user);
-            return { user: user };
-        }),
+  user: null,
+  setUser: (user) =>
+    set(() => {
+      // console.log('updating user : ', user);
+      return { user: user };
+    }),
 
-    userType: null,
-    setUserType: (userType) => set(() => ({ userType: userType })),
+  userType: null,
+  setUserType: (userType) => set(() => ({ userType: userType })),
 
-    userPlan: null,
-    setUserPlan: (userPlan) => set(() => ({ userPlan: userPlan })),
+  userPlan: null,
+  setUserPlan: (userPlan) => set(() => ({ userPlan: userPlan })),
 
-    token: null,
-    setToken: (token) => set(() => ({ token: token })),
+  token: null,
+  setToken: (token) => set(() => ({ token: token })),
 
-    currentInstituteId: null,
-    setCurrentInstituteId: (id) => set(() => ({ currentInstituteId: id })),
+  currentInstituteId: null,
+  setCurrentInstituteId: (id) => set(() => ({ currentInstituteId: id })),
 
-    institutes: [],
-    setInstitutes: (institutes) => set(() => ({ institutes: institutes })),
-    updateInstitute: (institute) =>
-        set((state) => {
-            const institutes = state.institutes.map((i) => {
-                if (i.institute_id === institute.institute_id) {
-                    return institute;
-                }
-                return i;
-            });
-            return { institutes };
-        }),
+  institutes: [],
+  setInstitutes: (institutes) => set(() => ({ institutes: institutes })),
+  updateInstitute: (institute) =>
+    set((state) => {
+      const institutes = state.institutes.map((i) => {
+        if (i.institute_id === institute.institute_id) {
+          return institute;
+        }
+        return i;
+      });
+      return { institutes };
+    }),
 }));
 
 export default useUserStore;
