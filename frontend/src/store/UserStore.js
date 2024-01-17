24d6364bfd08689b7ef7c77ec10fc48aee633790
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-01-17 20:38:24 +0530

    Updates

--- frontend/src/store/UserStore.js
+++ frontend/src/store/UserStore.js
@@ -1,40 +1,54 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 const useUserStore = create((set) => ({
-  user: null,
-  setUser: (user) =>
-    set(() => {
-      // console.log('updating user : ', user);
-      return { user: user };
-    }),
+	user: null,
+	setUser: (user) =>
+		set(() => {
+			// console.log('updating user : ', user);
+			return { user: user };
+		}),
 
-  userType: null,
-  setUserType: (userType) => set(() => ({ userType: userType })),
+	userPlan: null,
+	setUserPlan: (userPlan) => set(() => ({ userPlan: userPlan })),
 
-  userPlan: null,
-  setUserPlan: (userPlan) => set(() => ({ userPlan: userPlan })),
+	accessToken: null,
+	setAccessToken: (accessToken) => set(() => ({ accessToken: accessToken })),
 
-  accessToken: null,
-  setAccessToken: (accessToken) => set(() => ({ accessToken: accessToken })),
+	refreshToken: null,
+	setRefreshToken: (refreshToken) =>
+		set(() => ({ refreshToken: refreshToken })),
 
-  refreshToken: null,
-  setRefreshToken: (refreshToken) => set(() => ({ refreshToken: refreshToken })),
+	currentInstituteId: null,
+	setCurrentInstituteId: (id) => set(() => ({ currentInstituteId: id })),
 
-  currentInstituteId: null,
-  setCurrentInstituteId: (id) => set(() => ({ currentInstituteId: id })),
+	institutes: [],
+	setInstitutes: (institutes) => set(() => ({ institutes: institutes })),
+	updateInstitute: (institute) =>
+		set((state) => {
+			const institutes = state.institutes.map((i) => {
+				if (i.institute_id === institute.institute_id) {
+					return institute;
+				}
+				return i;
+			});
+			return { institutes };
+		}),
 
-  institutes: [],
-  setInstitutes: (institutes) => set(() => ({ institutes: institutes })),
-  updateInstitute: (institute) =>
-    set((state) => {
-      const institutes = state.institutes.map((i) => {
-        if (i.institute_id === institute.institute_id) {
-          return institute;
-        }
-        return i;
-      });
-      return { institutes };
-    }),
+	currentRole: null,
+	setCurrentRole: (role) => set(() => ({ currentRole: role })),
+
+	roles: [],
+	setRoles: (roles) => set(() => ({ roles: roles })),
+	updateRole: (role) =>
+		set((state) => {
+			const roles = state.roles.map((r) => {
+				if (r.role_id === role.role_id) {
+					return role;
+				}
+				return r;
+			});
+			return { roles };
+		}),
 }));
 
 export default useUserStore;
