c577bfa89feda9dcf43fc810d61c022f54bea9e5
smriti0302 <smriti030202@gmail.com>
2024-01-17 21:05:35 +0530

    update

--- backend/models/sql/UserRole.js
+++ backend/models/sql/UserRole.js
@@ -3,24 +3,36 @@ const { DataTypes } = require("sequelize");
 const { options } = require("./defaultOptions");
 const { User } = require("./User");
 const { Role } = require("./Role");
-const { Institute } = require( "./Institute" )
-const { Plan } = require( "./Plan" )
+const { Institute } = require("./Institute");
+const { Plan } = require("./Plan");
 
 const UserInstitutePlanRole = sequelize.define(
-	"user_institute_plan_role",
-	{
-		user_institute_plan_role_id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-	},
-	{ ...options }
+  "user_institute_plan_role",
+  {
+    user_institute_plan_role_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  { ...options }
 );
 
-UserRole.belongsTo(User, { foreignKey: "user_id", onDelete: "CASCADE" });
-UserRole.belongsTo(Role, { foreignKey: "role_id", onDelete: "SET NULL" });
-UserRole.belongsTo(Institute, { foreignKey: "institute_id", onDelete: "SET NULL" })
-UserRole.belongsTo(Plan, { foreignKey: "plan_id", onDelete: "SET NULL" })
+UserInstitutePlanRole.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+UserInstitutePlanRole.belongsTo(Role, {
+  foreignKey: "role_id",
+  onDelete: "SET NULL",
+});
+UserInstitutePlanRole.belongsTo(Institute, {
+  foreignKey: "institute_id",
+  onDelete: "SET NULL",
+});
+UserInstitutePlanRole.belongsTo(Plan, {
+  foreignKey: "plan_id",
+  onDelete: "SET NULL",
+});
 
 module.exports = { UserInstitutePlanRole };
