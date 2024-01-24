20a1e79f442d33a2b153e848c75507a8491ef22d
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-01-25 00:29:18 +0530

    Updated roles perms check v1

--- backend/models/sql/Permission.js
+++ backend/models/sql/Permission.js
@@ -1,28 +1,22 @@
-const { sequelize } = require('../../init.sequelize');
-const { DataTypes } = require('sequelize');
-const { options } = require('./defaultOptions');
-const { Role } = require('./Role');
+const { sequelize } = require("../../init.sequelize");
+const { DataTypes } = require("sequelize");
+const { options } = require("./defaultOptions");
+const { Role } = require("./Role");
 
 const Permission = sequelize.define(
-    'permission',
-    {
-        permission_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },
-    { ...options }
+	"permission",
+	{
+		permission_id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{ ...options }
 );
-
-Permission.belongsToMany(Role, {
-    through: 'role_permission',
-    sourceKey: 'permission_id',
-    targetKey: 'role_id',
-});
 
 module.exports = { Permission };
---
+++ backend/models/sql/RolePermission.js
@@ -0,0 +1,12 @@
+const { sequelize } = require("../../init.sequelize");
+const { DataTypes } = require("sequelize");
+const { options } = require("./defaultOptions");
+const { Role } = require("./Role");
+const { Permission } = require("./Permission");
+
+const RolePermission = sequelize.define("role_permission", {}, { ...options });
+
+RolePermission.belongsTo(Role, { foreignKey: "role_id" });
+RolePermission.belongsTo(Permission, { foreignKey: "permission_id" });
+
+module.exports = { RolePermission };
