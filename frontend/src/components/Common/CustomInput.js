bce7b9f750539688c6ac413d7231bebbe2782a0d
smriti0302 <smriti030202@gmail.com>
2024-01-25 00:05:06 +0530

    updates

--- frontend/src/components/Common/CustomInput.js
+++ frontend/src/components/Common/CustomInput.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 export default function CustomInput({ children, ...inputProps }) {
-    const [val, setVal] = useState(inputProps?.initialValue);
-    return (
-        <label htmlFor={inputProps?.name} className="flex flex-col gap-2">
-            <p className="text-sm text-gray-700">{children}</p>
-            <input
-                value={val}
-                onChange={(e) => setVal(e.target.value)}
-                {...inputProps}
-                className={
-                    inputProps?.type !== "text"
-                        ? "border rounded-lg p-1 focus:border-black transition-colors duration-200"
-                        : ""
-                }
-            />
-        </label>
-    );
+  const [val, setVal] = useState(inputProps?.initialValue);
+  return (
+    <label htmlFor={inputProps?.name} className="flex flex-col gap-2">
+      <p className="text-sm text-gray-700">{children}</p>
+      <input
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+        {...inputProps}
+        className={
+          inputProps?.type !== "text"
+            ? "border rounded-lg p-1 focus:border-black transition-colors duration-200"
+            : ""
+        }
+      />
+    </label>
+  );
 }
