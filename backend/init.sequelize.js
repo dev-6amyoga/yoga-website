4b1bd6291074349e785e3839e030e9b739d7ea79
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-01-25 00:28:41 +0530

    Added one device login

--- backend/init.sequelize.js
+++ backend/init.sequelize.js
@@ -5,25 +5,25 @@ const SQLite = require("sqlite3");
 dotenv.config();
 
 const DB_DATABASE = process.env.DB_DATABASE,
-  DB_USERNAME = process.env.DB_USERNAME,
-  DB_PASSWORD = process.env.DB_PASSWORD,
-  DB_HOST = process.env.DB_HOST,
-  DB_PORT = process.env.DB_PORT;
+	DB_USERNAME = process.env.DB_USERNAME,
+	DB_PASSWORD = process.env.DB_PASSWORD,
+	DB_HOST = process.env.DB_HOST,
+	DB_PORT = process.env.DB_PORT;
 
 // console.log(DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 var sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: "postgres",
-  logging: false,
-  host: DB_HOST,
-  port: DB_PORT,
-  dialectOptions: {},
-  benchmark: true
+	dialect: "postgres",
+	logging: false,
+	host: DB_HOST,
+	port: DB_PORT,
+	dialectOptions: {},
+	benchmark: true,
 });
 
 // Initializes and syncs the db
 function initializeSequelize() {
-  return sequelize.sync({ alter: true });
-  // return sequelize.sync({ force: true });
+	return sequelize.sync({ alter: true });
+	// return sequelize.sync({ force: true });
 }
 
 module.exports = { sequelize, initializeSequelize };
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const { User } = require("../models/sql/User");
 const { Role } = require("../models/sql/Role");
+const { HTTP_FORBIDDEN } = require("./http_status_codes");
 
 dotenv.config();
 
@@ -12,20 +13,40 @@ const TOKEN_TYPE_REFRESH = "REFRESH_TOKEN";
 
 function generateAccessToken(user) {
 	// TODO : update the expiry
-	return jwt.sign(
-		{ token_type: TOKEN_TYPE_ACCESS, user: user },
-		JWT_TOKEN_SECRET,
-		{ expiresIn: "300s" }
-	);
+
+	const now = new Date();
+
+	const exp = new Date();
+	exp.setSeconds(exp.getSeconds() + 300);
+
+	return [
+		jwt.sign(
+			{ token_type: TOKEN_TYPE_ACCESS, user: user },
+			JWT_TOKEN_SECRET,
+			{ expiresIn: "300s" }
+		),
+		now,
+		exp,
+	];
 }
 
 function generateRefreshToken(user) {
 	// TODO : update the expiry
-	return jwt.sign(
-		{ token_type: TOKEN_TYPE_REFRESH, user: user },
-		JWT_TOKEN_SECRET,
-		{ expiresIn: "1h" }
-	);
+
+	const now = new Date();
+
+	const exp = new Date();
+	exp.setHours(exp.getHours() + 1);
+
+	return [
+		jwt.sign(
+			{ token_type: TOKEN_TYPE_REFRESH, user: user },
+			JWT_TOKEN_SECRET,
+			{ expiresIn: "1h" }
+		),
+		now,
+		exp,
+	];
 }
 
 function verifyToken(token) {
@@ -40,21 +61,22 @@ function verifyToken(token) {
 	return [decoded, error];
 }
 
-function authenticateToken(req, res, next) {
-	const authHeader = req.headers["authorization"];
+async function authenticateToken(req, res, next) {
+	const authHeader = req.headers["token"];
 	// const refreshToken = req.cookies["refreshToken"];
-	const token = authHeader && authHeader.split(" ")[1];
+	const token = authHeader;
 
-	if (token == null) return res.sendStatus(401);
+	if (token === null || token === undefined) return res.sendStatus(401);
 
-	jwt.verify(token, JWT_TOKEN_SECRET, (err, decoded) => {
-		console.log(err);
+	jwt.verify(token, JWT_TOKEN_SECRET, async (err, decoded) => {
+		if (err) {
+			console.error(err.message);
+			return res.status(HTTP_FORBIDDEN).json({ message: "Forbidden" });
+		}
 
-		if (err) return res.sendStatus(403);
-
-		const u = User.findOne({
+		const u = await User.findOne({
 			where: {
-				username: user?.username,
+				username: decoded?.user?.username,
 			},
 			include: [
 				{
@@ -64,11 +86,11 @@ function authenticateToken(req, res, next) {
 			],
 		});
 
-		if (!u) res.sendStatus(403);
+		if (!u) res.status(HTTP_FORBIDDEN).json({ message: "Forbidden" });
 
 		// TODO : get roles and permissions
 
-		req.user = u;
+		req.user = u.toJSON();
 
 		next();
 
