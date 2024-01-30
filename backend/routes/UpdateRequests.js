3ea9778bc9e0c5002cd8807dd8f3918600b6af91
smriti0302 <smriti030202@gmail.com>
2024-01-30 13:45:44 +0530

    updates

--- backend/routes/UpdateRequests.js
+++ backend/routes/UpdateRequests.js
@@ -56,4 +56,93 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.get("/get-all", async (req, res) => {
+  try {
+    const updateRequests = await UpdateRequests.findAll();
+    res.status(HTTP_OK).json({ updateRequests });
+  } catch (error) {
+    console.error("Error fetching update requests:", error);
+    res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+      error: "Error fetching update requests",
+    });
+  }
+});
+
+router.post("/approve", async (req, res) => {
+  const { update_request_id } = req.body;
+
+  if (!update_request_id) {
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Missing required fields" });
+  }
+
+  const t = await sequelize.transaction();
+
+  try {
+    const updateRequest = await UpdateRequests.findByPk(update_request_id, {
+      transaction: t,
+    });
+
+    if (!updateRequest) {
+      await t.rollback();
+      return res
+        .status(HTTP_NOT_FOUND)
+        .json({ error: "Update request not found" });
+    }
+
+    updateRequest.is_approved = true;
+    await updateRequest.save({ transaction: t });
+    await t.commit();
+
+    return res
+      .status(HTTP_OK)
+      .json({ message: "Update request approved successfully" });
+  } catch (error) {
+    console.error("Error approving update request:", error);
+    await t.rollback();
+    return res
+      .status(HTTP_INTERNAL_SERVER_ERROR)
+      .json({ error: "Error approving update request" });
+  }
+});
+
+router.post("/reject", async (req, res) => {
+  const { update_request_id } = req.body;
+
+  if (!update_request_id) {
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Missing required fields" });
+  }
+
+  const t = await sequelize.transaction();
+
+  try {
+    const updateRequest = await UpdateRequests.findByPk(update_request_id, {
+      transaction: t,
+    });
+
+    if (!updateRequest) {
+      await t.rollback();
+      return res
+        .status(HTTP_NOT_FOUND)
+        .json({ error: "Update request not found" });
+    }
+
+    updateRequest.is_approved = false;
+    await updateRequest.save({ transaction: t });
+    await t.commit();
+
+    return res
+      .status(HTTP_OK)
+      .json({ message: "Update request rejected successfully" });
+  } catch (error) {
+    console.error("Error rejecting update request:", error);
+    await t.rollback();
+    return res
+      .status(HTTP_INTERNAL_SERVER_ERROR)
+      .json({ error: "Error rejecting update request" });
+  }
+});
 module.exports = router;
--- frontend/src/pages/admin/member-management/Students.js
+++ frontend/src/pages/admin/member-management/Students.js
@@ -1,11 +1,12 @@
 import { toast } from "react-toastify";
 import AdminNavbar from "../../../components/Common/AdminNavbar/AdminNavbar";
 import { Fetch } from "../../../utils/Fetch";
-import { Table } from "@geist-ui/core";
+import { Table, Card, Text, Grid, Button } from "@geist-ui/core";
 import { useEffect, useState } from "react";
 export default function Students() {
   const [students, setStudents] = useState([]);
   const [studentData, setStudentData] = useState([]);
+  const [updateRequests, setUpdateRequests] = useState([]);
   const appendToUsers = (newUserData) => {
     setStudentData((prevUsers) => [...prevUsers, newUserData]);
   };
@@ -51,18 +52,131 @@ export default function Students() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Fetch({
+          url: "http://localhost:4000/update-request/get-all",
+          method: "GET",
+        });
+        const data = response.data;
+        setUpdateRequests(data.updateRequests);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const RenderAction = (value, rowData, index) => {
+    const handleAccept = async () => {
+      Fetch({
+        url: "http://localhost:4000/update-request/approve",
+        method: "POST",
+        data: {
+          update_request_id: rowData.update_request_id,
+        },
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            toast("Email Update Approved", { type: "success" });
+            rowData.status = "ACCEPTED";
+            console.log(index);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          toast("An error occured!");
+        });
+    };
+    const handleReject = async () => {
+      Fetch({
+        url: "http://localhost:4000/update-request/reject",
+        method: "POST",
+        data: {
+          update_request_id: rowData.update_request_id,
+        },
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            toast("Email Update Rejected", { type: "success" });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          toast("An error occured!");
+        });
+    };
+    return (
+      <Grid.Container gap={0.1}>
+        <Grid>
+          <>
+            {rowData.status === "ACCEPTED" && <div>Hi</div>}
+            <Button
+              type="success"
+              auto
+              scale={1 / 3}
+              font="12px"
+              onClick={handleAccept}
+            >
+              Approve
+            </Button>
+            <Button
+              type="error"
+              auto
+              scale={1 / 3}
+              font="12px"
+              onClick={handleReject}
+            >
+              Reject
+            </Button>
+          </>
+        </Grid>
+      </Grid.Container>
+    );
+  };
+
   return (
-    <div className="allAsanas min-h-screen">
+    <div>
       <AdminNavbar />
-      <div className="elements">
-        <Table width={50} data={studentData} className="bg-white">
-          <Table.Column prop="user_id" label="ID" />
-          <Table.Column label="Student Name" width={150} prop="name" />
-          <Table.Column label="Email ID" width={150} prop="email" />
-          <Table.Column label="Phone" width={150} prop="phone" />
-          {/* <Table.Column label="Address 1" width={150} prop="address1" />
-          <Table.Column label="Address 2" width={150} prop="address2" /> */}
-        </Table>
+      <div className="allAsanas flex flex-col items-center min-h-screen justify-center">
+        <div className="elements">
+          <Table width="100%" data={studentData} className="bg-white">
+            <Table.Column prop="user_id" label="ID" />
+            <Table.Column label="Student Name" width={150} prop="name" />
+            <Table.Column label="Email ID" width={200} prop="email" />
+            <Table.Column label="Phone" width={150} prop="phone" />
+          </Table>
+        </div>
+
+        <Card>
+          <Text h4>Email Update Requests</Text>
+          <div className="flex flex-col items-start gap-2 p-6 bg-gray-500 text-white mt-4 mx-4 rounded-md">
+            <Card width="100%">
+              <Table width="100%" data={updateRequests} className="bg-white">
+                <Table.Column prop="user_id" label="User ID" />
+                <Table.Column label="Student Name" width={150} prop="name" />
+                <Table.Column
+                  label="Old Email ID"
+                  width={200}
+                  prop="old_email"
+                />
+                <Table.Column
+                  label="New Email ID"
+                  width={200}
+                  prop="new_email"
+                />
+                <Table.Column label="Phone" width={150} prop="phone" />
+                <Table.Column
+                  prop="operation"
+                  label="Approve/Reject"
+                  width={150}
+                  render={RenderAction}
+                />
+              </Table>
+            </Card>
+          </div>
+        </Card>
       </div>
     </div>
   );
