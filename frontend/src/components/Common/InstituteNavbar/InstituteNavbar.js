e124ceb4d72f34e0d51ae22631bbc775f2bca4dc
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-02-03 14:21:01 +0530

    Memoized nav

--- frontend/src/components/Common/InstituteNavbar/InstituteNavbar.js
+++ frontend/src/components/Common/InstituteNavbar/InstituteNavbar.js
@@ -1,175 +1,179 @@
-import { Button, ButtonDropdown, Drawer, Select } from "@geist-ui/core";
+import { Button, Drawer, Select } from "@geist-ui/core";
 import { Menu } from "@geist-ui/icons";
-import React, { useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import useUserStore from "../../../store/UserStore";
-import { useCookies } from "react-cookie";
-import { navigateToDashboard } from "../../../utils/navigateToDashboard";
 import RoleShifter from "../RoleShifter";
 
-export default function InstituteNavbar() {
-  const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
-  let user = useUserStore((state) => state.user);
-  const setUser = useUserStore((state) => state.setUser);
-  let institutes = useUserStore((state) => state.institutes);
+function InstituteNavbar() {
+	const navigate = useNavigate();
+	const [open, setOpen] = useState(false);
+	let user = useUserStore((state) => state.user);
+	const setUser = useUserStore((state) => state.setUser);
+	let institutes = useUserStore((state) => state.institutes);
 
-  let currentInstituteId = useUserStore((state) => state.currentInstituteId);
-  let setCurrentInstituteId = useUserStore(
-    (state) => state.setCurrentInstituteId
-  );
+	let currentInstituteId = useUserStore((state) => state.currentInstituteId);
+	let setCurrentInstituteId = useUserStore(
+		(state) => state.setCurrentInstituteId
+	);
 
-  const handleInstituteSelection = (value) => {
-    setCurrentInstituteId(parseInt(value));
-  };
+	const handleInstituteSelection = (value) => {
+		setCurrentInstituteId(parseInt(value));
+	};
 
-  const resetUserState = useUserStore((state) => state.resetUserState);
-  const [cookies, setCookie, removeCookie] = useCookies([
-    "6amyoga_access_token",
-    "6amyoga_refresh_token",
-  ]);
+	const resetUserState = useUserStore((state) => state.resetUserState);
+	const [cookies, setCookie, removeCookie] = useCookies([
+		"6amyoga_access_token",
+		"6amyoga_refresh_token",
+	]);
 
-  const handleLogout = () => {
-    removeCookie("6amyoga_access_token", { domain: "localhost", path: "/" });
-    removeCookie("6amyoga_refresh_token", { domain: "localhost", path: "/" });
-    resetUserState();
-    navigate("/auth");
-  };
+	const handleLogout = () => {
+		removeCookie("6amyoga_access_token", {
+			domain: "localhost",
+			path: "/",
+		});
+		removeCookie("6amyoga_refresh_token", {
+			domain: "localhost",
+			path: "/",
+		});
+		resetUserState();
+		navigate("/auth");
+	};
 
-  useEffect(() => {}, [user]);
+	useEffect(() => {}, [user]);
 
-  return (
-    <div>
-      <div className="w-full px-4 py-1 flex bg-zinc-800 text-white items-center gap-4">
-        <Button
-          width={"100%"}
-          auto
-          ghost
-          onClick={() => setOpen(true)}
-          icon={<Menu />}
-        />
-        <p className="font-bold text-xl">6AM Yoga</p>
-      </div>
-      <Drawer visible={open} onClose={() => setOpen(false)} placement="left">
-        <Drawer.Title>6AM Yoga</Drawer.Title>
-        <Drawer.Subtitle>Institute Dashboard</Drawer.Subtitle>
-        <Drawer.Content>
-          <div className="py-4">
-            <RoleShifter />
-            <Select
-              width="100%"
-              value={String(currentInstituteId)}
-              placeholder="Select An Institute"
-              onChange={handleInstituteSelection}
-            >
-              {institutes?.map((institute) => {
-                return (
-                  <Select.Option
-                    key={institute.institute_id}
-                    value={String(institute.institute_id)}
-                  >
-                    {institute.name}
-                  </Select.Option>
-                );
-              })}
-            </Select>
-          </div>
-          <div className="flex flex-col gap-4 w-full">
-            <Button className="w-full">
-              <Link to={"/institute"} className="w-full text-zinc-800">
-                Dashboard
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/purchase-a-plan"}
-                className="w-full text-zinc-800"
-              >
-                Purchase A Plan
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/member-management"}
-                className="w-full text-zinc-800"
-              >
-                Member Management
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/add-new-teacher"}
-                className="w-full text-zinc-800"
-              >
-                Add New Teacher
-              </Link>
-            </Button>
-            <Button
-              onClick={() => {
-                navigate("/institute/playlist-page");
-              }}
-            >
-              Playlist Page
-            </Button>
-            <Button>
-              <Link
-                to={"/institute/make-playlist"}
-                className="w-full text-zinc-800"
-              >
-                Make New Playlist
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/add-new-teacher"}
-                className="w-full text-zinc-800"
-              >
-                Reports
-              </Link>
-            </Button>
-            <hr />
-            <Button className="w-full">
-              <Link to={"/institute/settings"} className="w-full text-zinc-800">
-                Institute Settings
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/user/settings"}
-                className="w-full text-zinc-800"
-              >
-                User Settings
-              </Link>
-            </Button>
-            <Button className="w-full">
-              <Link
-                to={"/institute/view-transactions"}
-                className="w-full text-zinc-800"
-              >
-                View Transactions
-              </Link>
-            </Button>
+	return (
+		<div>
+			<div className="w-full px-4 py-1 flex bg-zinc-800 text-white items-center gap-4">
+				<Button
+					width={"100%"}
+					auto
+					ghost
+					onClick={() => setOpen(true)}
+					icon={<Menu />}
+				/>
+				<p className="font-bold text-xl">6AM Yoga</p>
+			</div>
+			<Drawer
+				visible={open}
+				onClose={() => setOpen(false)}
+				placement="left">
+				<Drawer.Title>6AM Yoga</Drawer.Title>
+				<Drawer.Subtitle>Institute Dashboard</Drawer.Subtitle>
+				<Drawer.Content>
+					<div className="py-4">
+						<RoleShifter />
+						<Select
+							width="100%"
+							value={String(currentInstituteId)}
+							placeholder="Select An Institute"
+							onChange={handleInstituteSelection}>
+							{institutes?.map((institute) => {
+								return (
+									<Select.Option
+										key={institute.institute_id}
+										value={String(institute.institute_id)}>
+										{institute.name}
+									</Select.Option>
+								);
+							})}
+						</Select>
+					</div>
+					<div className="flex flex-col gap-4 w-full">
+						<Button className="w-full">
+							<Link
+								to={"/institute"}
+								className="w-full text-zinc-800">
+								Dashboard
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/purchase-a-plan"}
+								className="w-full text-zinc-800">
+								Purchase A Plan
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/member-management"}
+								className="w-full text-zinc-800">
+								Member Management
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/add-new-teacher"}
+								className="w-full text-zinc-800">
+								Add New Teacher
+							</Link>
+						</Button>
+						<Button
+							onClick={() => {
+								navigate("/institute/playlist-page");
+							}}>
+							Playlist Page
+						</Button>
+						<Button>
+							<Link
+								to={"/institute/make-playlist"}
+								className="w-full text-zinc-800">
+								Make New Playlist
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/add-new-teacher"}
+								className="w-full text-zinc-800">
+								Reports
+							</Link>
+						</Button>
+						<hr />
+						<Button className="w-full">
+							<Link
+								to={"/institute/settings"}
+								className="w-full text-zinc-800">
+								Institute Settings
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/user/settings"}
+								className="w-full text-zinc-800">
+								User Settings
+							</Link>
+						</Button>
+						<Button className="w-full">
+							<Link
+								to={"/institute/view-transactions"}
+								className="w-full text-zinc-800">
+								View Transactions
+							</Link>
+						</Button>
 
-            <hr />
-            {user ? (
-              <>
-                <h2 className="text-sm text-center">
-                  Logged in as {user?.name}
-                </h2>
-                <Button type="error" onClick={handleLogout}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <Link to={"/auth"} className="w-full">
-                <Button type="primary" width="100%">
-                  Login
-                </Button>
-              </Link>
-            )}
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </div>
-  );
+						<hr />
+						{user ? (
+							<>
+								<h2 className="text-sm text-center">
+									Logged in as {user?.name}
+								</h2>
+								<Button type="error" onClick={handleLogout}>
+									Logout
+								</Button>
+							</>
+						) : (
+							<Link to={"/auth"} className="w-full">
+								<Button type="primary" width="100%">
+									Login
+								</Button>
+							</Link>
+						)}
+					</div>
+				</Drawer.Content>
+			</Drawer>
+		</div>
+	);
 }
+
+export default memo(InstituteNavbar);
--- frontend/src/components/Common/TeacherNavbar/TeacherNavbar.js
+++ frontend/src/components/Common/TeacherNavbar/TeacherNavbar.js
@@ -1,116 +1,131 @@
 import { Button, Drawer } from "@geist-ui/core";
 import { Menu, User } from "@geist-ui/icons";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import useUserStore from "../../../store/UserStore";
+import { memo, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import { navigateToDashboard } from "../../../utils/navigateToDashboard";
-import RoleShifter from "../RoleShifter";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import useUserStore from "../../../store/UserStore";
+import RoleShifter from "../RoleShifter";
 
-export default function TeacherNavbar() {
-  const [open, setOpen] = useState(false);
-  const navigate = useNavigate();
-  let user = useUserStore((state) => state.user);
-  const setUser = useUserStore((state) => state.setUser);
+function TeacherNavbar() {
+	const [open, setOpen] = useState(false);
+	const navigate = useNavigate();
+	let user = useUserStore((state) => state.user);
+	const setUser = useUserStore((state) => state.setUser);
 
-  const [planId, setPlanId] = useState(0);
-  const [playDisabled, setPlayDisabled] = useState(true);
-  const [tailorMadeDisabled, setTailorMadeDisabled] = useState(true);
-  const [selfAudioDisabled, setSelfAudioDisabled] = useState(true);
+	const [planId, setPlanId] = useState(0);
+	const [playDisabled, setPlayDisabled] = useState(true);
+	const [tailorMadeDisabled, setTailorMadeDisabled] = useState(true);
+	const [selfAudioDisabled, setSelfAudioDisabled] = useState(true);
 
-  const resetUserState = useUserStore((state) => state.resetUserState);
+	const resetUserState = useUserStore((state) => state.resetUserState);
 
-  const [cookies, setCookie, removeCookie] = useCookies([
-    "6amyoga_access_token",
-    "6amyoga_refresh_token",
-  ]);
+	const [cookies, setCookie, removeCookie] = useCookies([
+		"6amyoga_access_token",
+		"6amyoga_refresh_token",
+	]);
 
-  const handleLogout = () => {
-    removeCookie("6amyoga_access_token", { domain: "localhost", path: "/" });
-    removeCookie("6amyoga_refresh_token", { domain: "localhost", path: "/" });
-    resetUserState();
-    navigate("/auth");
-  };
+	const handleLogout = () => {
+		removeCookie("6amyoga_access_token", {
+			domain: "localhost",
+			path: "/",
+		});
+		removeCookie("6amyoga_refresh_token", {
+			domain: "localhost",
+			path: "/",
+		});
+		resetUserState();
+		navigate("/auth");
+	};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "http://localhost:4000/user-plan/get-user-plan-by-id",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ user_id: user?.user_id }),
-          }
-        );
-        const data = await response.json();
-        setPlayDisabled(!data?.userPlan?.plan.has_basic_playlist);
-        setTailorMadeDisabled(!data?.userPlan?.plan.has_playlist_creation);
-        setSelfAudioDisabled(!data?.userPlan?.plan.has_self_audio_upload);
-        const retrievedPlanId = data?.userPlan?.plan_id;
-      } catch (err) {
-        toast(err);
-      }
-    };
-    fetchData();
-  }, [user]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:4000/user-plan/get-user-plan-by-id",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ user_id: user?.user_id }),
+					}
+				);
+				const data = await response.json();
+				setPlayDisabled(!data?.userPlan?.plan.has_basic_playlist);
+				setTailorMadeDisabled(
+					!data?.userPlan?.plan.has_playlist_creation
+				);
+				setSelfAudioDisabled(
+					!data?.userPlan?.plan.has_self_audio_upload
+				);
+				const retrievedPlanId = data?.userPlan?.plan_id;
+			} catch (err) {
+				toast(err);
+			}
+		};
+		fetchData();
+	}, [user]);
 
-  return (
-    <>
-      <div className="w-full px-4 py-1 flex bg-zinc-800 text-white items-center gap-4">
-        <Button
-          width={"100%"}
-          auto
-          ghost
-          onClick={() => setOpen(true)}
-          icon={<Menu />}
-        />
-        <p className="font-bold text-xl">6AM Yoga</p>
-      </div>
-      <Drawer visible={open} onClose={() => setOpen(false)} placement="left">
-        <Drawer.Title>
-          <p className="font-bold text-xl">6AM Yoga</p>
-        </Drawer.Title>
-        <Drawer.Subtitle>Teacher Dashboard</Drawer.Subtitle>
-        <hr />
-        <Drawer.Content>
-          <div className="flex flex-col gap-4">
-            <RoleShifter />
-            <Button onClick={() => navigate("/teacher")}>Dashboard</Button>
-            <Button>Free Videos</Button>
-            <Button
-              disabled={playDisabled}
-              onClick={() => navigate("/teacher/playlist")}
-            >
-              Playlist Player
-            </Button>
-            <Button
-              disabled={tailorMadeDisabled}
-              onClick={() => navigate("/teacher/make-playlist")}
-            >
-              Make your own Playlist
-            </Button>
-            <Button
-              disabled={selfAudioDisabled}
-              onClick={() => navigate("/teacher/self-audio-upload")}
-            >
-              Self Audio Upload
-            </Button>
-            <Button>About Us</Button>
-            <Button>Contact Us</Button>
-            <hr />
-            <Button icon={<User />} type="success" ghost>
-              {user?.name.split(" ")[0]}
-            </Button>
-            <Button type="error" onClick={handleLogout}>
-              Logout
-            </Button>
-          </div>
-        </Drawer.Content>
-      </Drawer>
-    </>
-  );
+	return (
+		<>
+			<div className="w-full px-4 py-1 flex bg-zinc-800 text-white items-center gap-4">
+				<Button
+					width={"100%"}
+					auto
+					ghost
+					onClick={() => setOpen(true)}
+					icon={<Menu />}
+				/>
+				<p className="font-bold text-xl">6AM Yoga</p>
+			</div>
+			<Drawer
+				visible={open}
+				onClose={() => setOpen(false)}
+				placement="left">
+				<Drawer.Title>
+					<p className="font-bold text-xl">6AM Yoga</p>
+				</Drawer.Title>
+				<Drawer.Subtitle>Teacher Dashboard</Drawer.Subtitle>
+				<hr />
+				<Drawer.Content>
+					<div className="flex flex-col gap-4">
+						<RoleShifter />
+						<Button onClick={() => navigate("/teacher")}>
+							Dashboard
+						</Button>
+						<Button>Free Videos</Button>
+						<Button
+							disabled={playDisabled}
+							onClick={() => navigate("/teacher/playlist")}>
+							Playlist Player
+						</Button>
+						<Button
+							disabled={tailorMadeDisabled}
+							onClick={() => navigate("/teacher/make-playlist")}>
+							Make your own Playlist
+						</Button>
+						<Button
+							disabled={selfAudioDisabled}
+							onClick={() =>
+								navigate("/teacher/self-audio-upload")
+							}>
+							Self Audio Upload
+						</Button>
+						<Button>About Us</Button>
+						<Button>Contact Us</Button>
+						<hr />
+						<Button icon={<User />} type="success" ghost>
+							{user?.name.split(" ")[0]}
+						</Button>
+						<Button type="error" onClick={handleLogout}>
+							Logout
+						</Button>
+					</div>
+				</Drawer.Content>
+			</Drawer>
+		</>
+	);
 }
+
+export default memo(TeacherNavbar);
