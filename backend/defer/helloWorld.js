86d4f9b4f9d592c42b7eb3afd81e1ac51b3f0602
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-02-10 23:35:36 +0530

    Defer init

---
+++ backend/defer/helloWorld.js
@@ -0,0 +1,12 @@
+const Defer = require("@defer/client");
+
+const helloWorld = async (name) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			console.log(`Hello ${name}!`);
+			resolve("done");
+		}, 5000);
+	});
+};
+
+module.exports = Defer.defer(helloWorld);
