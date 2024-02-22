1084b6e7c4e1950fa60d18576f54bf0eb99d492f
smriti0302 <smriti030202@gmail.com>
2024-02-22 21:05:47 +0530

    schedule

--- frontend/src/components/Sidebar/PlaylistItem.js
+++ frontend/src/components/Sidebar/PlaylistItem.js
@@ -1,22 +1,22 @@
-import { Button } from "@geist-ui/core";
-import { memo } from "react";
+import { Button } from '@geist-ui/core'
+import { memo } from 'react'
 
 function PlaylistItem({ playlist, add, deets }) {
-	return (
-		<div className="px-2 py-2 rounded-xl border border-zinc-800">
-			<div className="flex justify-between gap-8 items-center">
-				<p>{playlist.playlist_name}</p>
-				<div className="flex gap-4 items-center scale-75">
-					<Button auto type="outline" onClick={add}>
-						Add
-					</Button>
-					<Button auto type="outline" onClick={deets}>
-						Details
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="px-2 py-2 rounded-xl border border-zinc-800">
+            <div className="flex justify-between gap-8 items-center">
+                <p>{playlist.playlist_name || playlist.schedule_name}</p>
+                <div className="flex gap-4 items-center scale-75">
+                    <Button auto type="outline" onClick={add}>
+                        Add
+                    </Button>
+                    <Button auto type="outline" onClick={deets}>
+                        Details
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default memo(PlaylistItem);
+export default memo(PlaylistItem)
--- frontend/src/pages/teacher/PlanAllocator.js
+++ frontend/src/pages/teacher/PlanAllocator.js
@@ -1,28 +1,43 @@
 import { toast } from 'react-toastify'
 import { Fetch } from '../../utils/Fetch'
+import { useState } from 'react'
 export const PlanAllocator = (teacher_id, institute_id) => {
     //get institute owner
     //get user plan which is active
+    const [instituteOwnerId, setInstituteOwnerId] = useState(0)
+    const [planId, setPlanId] = useState(0)
     const fetchData = async () => {
         if (institute_id > 0) {
             try {
-                const response = await Fetch({
-                    url: 'http://localhost:4000/user-plan/get-user-institute-plan-by-id',
+                Fetch({
+                    url: 'http://localhost:4000/user/get-by-instituteid',
                     method: 'POST',
                     data: {
                         institute_id: institute_id,
                     },
+                }).then((res) => {
+                    if (res.status === 200) {
+                        setInstituteOwnerId(res.data.users[0].user_id)
+                        toast('User Plan Fetched')
+                        Fetch({
+                            url: 'http://localhost:4000/user-plan/get-active-user-plan-by-id',
+                            method: 'POST',
+                            data: {
+                                user_id: res.data.users[0].user_id,
+                            },
+                        }).then((res) => {
+                            if (res.status === 200) {
+                                setPlanId(res.data.userPlan[0].plan_id)
+                                console.log(res.data.userPlan[0])
+                                toast('User Plan Fetched')
+                            }
+                        })
+                    }
                 })
-                if (response.status === 200) {
-                    toast('User Plan Fetched')
-                } else {
-                    toast('Failed')
-                }
             } catch (error) {
                 console.error('Error fetching data:', error)
             }
         }
     }
     fetchData()
-    //get plan details from user plan
 }
