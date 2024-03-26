66e602bb5a9f31724b9c253e778305e64873f6ae
smriti0302 <smriti030202@gmail.com>
2024-03-26 23:26:47 +0530

    update10101

--- frontend/src/pages/testing/Video.js
+++ frontend/src/pages/testing/Video.js
@@ -2,20 +2,47 @@ import { useEffect } from "react";
 import Playlist from "../../components/Sidebar/Playlist";
 import VideoPlayerWrapper from "../../components/StackVideoShaka/VideoPlayerWrapper";
 import useWatchHistoryStore from "../../store/WatchHistoryStore";
+import { toast } from "react-toastify";
 
 export default function TestingVideo() {
-	const setEnableWatchHistory = useWatchHistoryStore(
-		(state) => state.setEnableWatchHistory
-	);
+  const setEnableWatchHistory = useWatchHistoryStore(
+    (state) => state.setEnableWatchHistory
+  );
 
-	useEffect(() => {
-		setEnableWatchHistory(false);
-	}, [setEnableWatchHistory]);
+  //   wait click middle button well see which jey it maps to
 
-	return (
-		<div className="mx-auto my-20 max-w-7xl p-4 xl:p-0">
-			<VideoPlayerWrapper />
-			<Playlist />
-		</div>
-	);
+  useEffect(() => {
+    // for hand held pointer
+    const handleKeyDown = (event) => {
+      event.preventDefault();
+      switch (event.key) {
+        case "PageUp":
+          console.log("Left arrow key pressed!");
+          break;
+        case "PageDown":
+          console.log("Right arrow key pressed!");
+          break;
+        default:
+          break;
+      }
+      console.log("keyDown", event);
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    setEnableWatchHistory(false);
+  }, [setEnableWatchHistory]);
+
+  return (
+    <div className="mx-auto my-20 max-w-7xl p-4 xl:p-0">
+      <VideoPlayerWrapper />
+      <Playlist />
+    </div>
+  );
 }
