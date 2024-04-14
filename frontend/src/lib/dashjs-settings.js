44ff41ed517f1bc48a2babac793f963101152c36
smriti0302 <smriti030202@gmail.com>
2024-04-14 11:53:33 +0530

    pls

--- frontend/src/lib/dashjs-settings.js
+++ frontend/src/lib/dashjs-settings.js
@@ -1,13 +1,13 @@
 export const dashSettings = {
-	debug: {
-		logLevel: 5,
-	},
-	streaming: {
-		cacheInitSegments: true,
-		buffer: {
-			initialBufferLevel: 4,
-			bufferTimeAtTopQuality: 15,
-			bufferToKeep: 10,
-		},
-	},
+  debug: {
+    logLevel: 5,
+  },
+  streaming: {
+    cacheInitSegments: true,
+    buffer: {
+      initialBufferLevel: 4,
+      bufferTimeAtTopQuality: 15,
+      bufferToKeep: 10,
+    },
+  },
 };
