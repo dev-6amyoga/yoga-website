6fd8c7268e2ad8b8beaafb28fe39607dd5f325bd
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-04-23 22:15:48 +0530

    Init for class mode

---
+++ syncer-backend/go.mod
@@ -0,0 +1,8 @@
+module syncer-backend
+
+go 1.21.0
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
---
+++ syncer-backend/src/server/types.go
@@ -0,0 +1,12 @@
+package server
+
+import "go.uber.org/zap"
+
+type Server struct {
+	// socker server instance
+
+	// db
+
+	// logger
+	logger *zap.SugaredLogger
+}
