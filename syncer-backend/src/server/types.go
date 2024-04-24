676a344b093fa9232f034836e9be82c00094636c
smriti0302 <smriti030202@gmail.com>
2024-04-24 20:51:50 +0530

    mongo connect

--- syncer-backend/src/server/types.go
+++ syncer-backend/src/server/types.go
@@ -1,11 +1,13 @@
 package server
 
 import "go.uber.org/zap"
+import "go.mongodb.org/mongo-driver/mongo"
 
 type Server struct {
 	// socker server instance
 
 	// db
+	dbClient *mongo.Client
 
 	// logger
 	logger *zap.SugaredLogger
