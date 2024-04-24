85984c1c17d7a398899dcc7a890af85323a1b7e2
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-04-25 00:23:41 +0530

    Added events, updated handlers

--- syncer-backend/src/server/connection.go
+++ syncer-backend/src/server/connection.go
@@ -1,6 +1,92 @@
 package server
 
-import "net"
+import (
+	"encoding/json"
+	"net/http"
+	"syncer-backend/src/events"
+)
 
-func (s *Server) handleConnection(conn net.Conn) {
+/*
+Teacher connection handler
+- Handles the WebSocket connection for the teacher
+- Stores the messages in the db
+*/
+func (s *Server) handleTeacherConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		s.logger.Error("Error upgrading to WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		event := events.Event{}
+
+		err = json.Unmarshal(message, &event)
+
+		if err != nil {
+			s.logger.Error("Error unmarshalling message:", err)
+			return
+		}
+
+		switch event.Type {
+		case events.EVENT_QUEUE:
+			queueEvent := events.QueueEvent{}
+			err = json.Unmarshal(message, &queueEvent)
+
+			if err != nil {
+				conn.WriteJSON(events.EVENT_NACK)
+				s.logger.Error("Error unmarshalling message:", err)
+				return
+			}
+		case events.EVENT_CONTROLS:
+			controlsEvent := events.ControlsEvent{}
+
+			err = json.Unmarshal(message, &controlsEvent)
+
+			if err != nil {
+				conn.WriteJSON(events.EVENT_NACK)
+				s.logger.Error("Error unmarshalling message:", err)
+				return
+			}
+		}
+
+		conn.WriteJSON(events.EVENT_ACK)
+	}
+
+}
+
+/*
+Student connection handler
+- Handles the WebSocket connection for the student
+- Polls for events from the db and sends to students
+*/
+func (s *Server) handleStudentConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		s.logger.Error("Error upgrading to WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// poll for events
+		// send events to students
+		msg := []byte("{'message': 'Hello'}")
+
+		err := conn.WriteJSON(msg)
+
+		if err != nil {
+			conn.WriteJSON(events.EVENT_NACK)
+			s.logger.Error("Error writing to WebSocket:", err)
+			return
+		}
+
+		conn.WriteJSON(msg)
+	}
 }
--- syncer-backend/src/server/server.go
+++ syncer-backend/src/server/server.go
@@ -1,50 +1,50 @@
 package server
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"time"
 	"context"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
-    "github.com/gorilla/websocket"
-    "github.com/google/uuid"  
 )
 
 func NewServer() *Server {
 	l, err := zap.NewDevelopment()
 
+	if err != nil {
+		panic(err)
+	}
+
 	prependedLogger := l.Sugar()
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://smriti030202:pass,123@yogawebsite.lxvodui.mongodb.net/YogaWebsite"))
+	client, err := mongo.Connect(
+		context.Background(),
+		options.Client().ApplyURI("mongodb+srv://smriti030202:pass,123@yogawebsite.lxvodui.mongodb.net/YogaWebsite"),
+	)
 
 	if err != nil {
 		panic(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
 	if err != nil {
 		prependedLogger.Errorf("Error connecting to MongoDB: %s", err)
-		panic(err) 
+		panic(err)
 	}
 
 	// Check if the connection was successful
-	err = client.Ping(ctx, nil)
+	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		prependedLogger.Errorf("Error pinging MongoDB: %s", err)
-		panic(err) 
+		panic(err)
 	}
 
 	prependedLogger.Info("Successfully connected to MongoDB")
 
 	return &Server{
 		dbClient: client,
-		logger: prependedLogger,
+		logger:   prependedLogger,
 	}
 }
 
@@ -53,63 +53,12 @@ func (s *Server) Start() {
 	s.logger.Info("Server started at port ----")
 
 	// serve
-	tcpServer, err := net.Listen("tcp", ":9209")
+	http.HandleFunc("/teacher/ws", s.handleTeacherConnection)
+	http.HandleFunc("/student/ws", s.handleStudentConnection)
 
-	if err != nil {
-		s.logger.Error("Error starting server")
-		panic(err)
-	}
-	for {
-		connection, err := tcpServer.Accept()
+	go http.ListenAndServe(":8080", nil) // Example: Port 8080
 
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
-		}
-
-		fmt.Println("client connected")
-
-		go s.handleConnection(connection)
-	}
-
-	http.HandleFunc("/ws", s.handleWebSocket) 
-    go http.ListenAndServe(":8080", nil) // Example: Port 8080
-    s.logger.Info("HTTP server for WebSocket started at port 8080")  
-
+	s.logger.Info("HTTP server for WebSocket started at port 8080")
 }
 
-var upgrader = websocket.Upgrader{} 
-
-func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil) 
-    if err != nil {
-        s.logger.Error("Error upgrading to WebSocket:", err)
-        return
-    }
-    defer conn.Close() 
-
-    urlID := r.URL.Query().Get("url") 
-    if urlID == "" {
-        return
-    }
-    s.socketGroups[urlID] = append(s.socketGroups[urlID], conn) 
-    s.logger.Infof("Client joined URL group: %s", urlID)
-
-	for {
-        _, message, err := conn.ReadMessage()
-        if err != nil {
-            break 
-        }
-        s.broadcastToGroup(urlID, message)
-    }
-
-}
-
-func (s *Server) broadcastToGroup(urlID string, message []byte) {
-    group := s.socketGroups[urlID]
-    for _, conn := range group {
-        if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-            panic(err)
-        }
-    }
-}
+var upgrader = websocket.Upgrader{}
