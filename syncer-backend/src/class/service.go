5af1e9b325c070104613502126a327c70c7ebe89
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-05-18 01:08:03 +0530

    Updates to syncer

---
+++ syncer-backend/src/class/service.go
@@ -0,0 +1,14 @@
+package class
+
+func NewClass() (*ClassModel, error) {
+	return nil, nil
+}
+
+func AddToActionsQueue() {
+}
+
+func AddToControlsQueue() {
+}
+
+func AddToWatchHistory() {
+}
--- syncer-backend/src/events/types.go
+++ syncer-backend/src/events/types.go
@@ -3,13 +3,21 @@ package events
 // Event types
 
 /*
+Queue Event Types
+- EVENT_QUEUE_PUSH
+- EVENT_QUEUE_POP
+- EVENT_QUEUE_CLEAR
+
+*/
+
+/*
 Example :
 {
 	type: "EVENT_QUEUE",
 	data: {
-		subtype: "EVENT_QUEUE_ADD",
+		subtype: "EVENT_QUEUE_PUSH",
 		data: {}
-	}
+	},
 }
 */
 
@@ -18,43 +26,57 @@ type EventType string
 const (
 	EVENT_QUEUE    EventType = "EVENT_QUEUE"
 	EVENT_CONTROLS EventType = "EVENT_CONTROLS"
+	EVENT_TIMER    EventType = "EVENT_TIMER"
 )
 
 type Event struct {
-	Type EventType `json:"type"`
-	Data struct{}  `json:"data"`
+	// objectID string of the class in mongodb
+	ClassID string    `json:"class_id"`
+	Type    EventType `json:"type"`
+	Data    struct{}  `json:"data"`
 }
 
+// ----------------------------------------------------------
 type QueueEventType string
 
 const (
-	EVENT_QUEUE_ADD QueueEventType = "EVENT_QUEUE_ADD"
-	EVENT_QUEUE_POP QueueEventType = "EVENT_QUEUE_POP"
+	EVENT_QUEUE_PUSH  QueueEventType = "EVENT_QUEUE_PUSH"
+	EVENT_QUEUE_POP   QueueEventType = "EVENT_QUEUE_POP"
+	EVENT_QUEUE_CLEAR QueueEventType = "EVENT_QUEUE_CLEAR"
 )
 
 type QueueEvent struct {
-	SubType QueueEventType `json:"subtype"`
-	Data    struct{}       `json:"data"`
+	SubType   QueueEventType `json:"subtype"`
+	Data      struct{}       `json:"data"`
+	EventTime string         `json:"event_time"`
 }
 
+// ----------------------------------------------------------
 type ControlsEventType string
 
 const (
 	EVENT_CONTROLS_PLAY        ControlsEventType = "EVENT_CONTROLS_PLAY"
 	EVENT_CONTROLS_PAUSE       ControlsEventType = "EVENT_CONTROLS_PAUSE"
-	EVENT_CONTROLS_VOLUME      ControlsEventType = "EVENT_CONTROLS_VOLUME"
-	EVENT_CONTROLS_SEEK_TO     ControlsEventType = "EVENT_CONTROLS_SEEK_TO"
-	EVENT_CONTROLS_SEEK_MARKER ControlsEventType = "EVENT_CONTROLS_SEEK_MARKER"
-	EVENT_CONTROLS_LOOP        ControlsEventType = "EVENT_CONTROLS_LOOP"
 	EVENT_CONTROLS_NEXT        ControlsEventType = "EVENT_CONTROLS_NEXT"
 	EVENT_CONTROLS_PREV        ControlsEventType = "EVENT_CONTROLS_PREV"
+	EVENT_CONTROLS_SEEK_TO     ControlsEventType = "EVENT_CONTROLS_SEEK_TO"
+	EVENT_CONTROLS_SEEK_MARKER ControlsEventType = "EVENT_CONTROLS_SEEK_MARKER"
 )
 
 type ControlsEvent struct {
-	SubType ControlsEventType `json:"subtype"`
-	Data    struct{}          `json:"data"`
+	SubType   ControlsEventType `json:"subtype"`
+	Data      struct{}          `json:"data"`
+	EventTime string            `json:"event_time"`
 }
 
+// ----------------------------------------------------------
+
+type TimerEvent struct {
+	CurrentTime float32 `json:"current_time"`
+	EventTime   string  `json:"event_time"`
+}
+
+// ----------------------------------------------------------
 type EventStatus string
 
 const (
@@ -62,11 +84,12 @@ const (
 	EVENT_STATUS_NACK EventStatus = "EVENT_STATUS_NACK"
 )
 
-type EventACK struct {
+type EventStudentResponse struct {
 	Status EventStatus `json:"status"`
+	Data   []Event     `json:"data"`
 }
 
-var (
-	EVENT_ACK  = EventACK{Status: EVENT_STATUS_ACK}
-	EVENT_NACK = EventACK{Status: EVENT_STATUS_NACK}
-)
+type EventTeacherResponse struct {
+	Status  EventStatus `json:"status"`
+	Message string      `json:"message"`
+}
