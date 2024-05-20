31b59b602cbe5e8f3fbb7226466fbf21d32cc406
smriti0302 <smriti030202@gmail.com>
2024-05-20 17:24:33 +0530

    mediapipe

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,3 +8,108 @@ module.exports = {
     enable: null,
   },
 };
+
+// const million = require("million/compiler");
+
+// module.exports = {
+//   webpack: {
+//     plugins: {
+//       add: [million.webpack({ auto: true })],
+//     },
+//     configure: (webpackConfig, { env, paths }) => {
+//       webpackConfig.module.rules.push({
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env", "@babel/preset-react"],
+//             plugins: [
+//               [
+//                 "@babel/plugin-transform-react-jsx",
+//                 { throwIfNamespace: false },
+//               ],
+//             ],
+//           },
+//         },
+//       });
+
+//       return webpackConfig;
+//     },
+//   },
+//   eslint: {
+//     enable: null,
+//   },
+//   babel: {
+//     presets: ["@babel/preset-env", "@babel/preset-react"],
+//     plugins: [
+//       ["@babel/plugin-transform-react-jsx", { throwIfNamespace: false }],
+//     ],
+//   },
+// };
+
+// const million = require("million/compiler");
+
+// module.exports = {
+//   webpack: {
+//     plugins: {
+//       add: [million.webpack({ auto: true })],
+//     },
+//     configure: (webpackConfig, { env, paths }) => {
+//       const babelLoaderRule = {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: [
+//               ["@babel/preset-env"],
+//               ["@babel/preset-react", { runtime: "automatic" }],
+//             ],
+//             plugins: [
+//               [
+//                 "@babel/plugin-transform-react-jsx",
+//                 { runtime: "automatic", throwIfNamespace: false },
+//               ],
+//             ],
+//           },
+//         },
+//       };
+
+//       // Find the existing rule for JS/JSX files and modify it
+//       webpackConfig.module.rules.push({
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules\/(?!solid-icons)/,
+//         use: babelLoaderRule.use,
+//       });
+
+//       // Make sure source-map-loader ignores problematic files
+//       webpackConfig.module.rules.forEach((rule) => {
+//         if (
+//           rule.use &&
+//           rule.use[0] &&
+//           rule.use[0].loader === "source-map-loader"
+//         ) {
+//           rule.exclude = [/node_modules\/solid-icons/];
+//         }
+//       });
+
+//       return webpackConfig;
+//     },
+//   },
+//   eslint: {
+//     enable: null,
+//   },
+//   babel: {
+//     presets: [
+//       ["@babel/preset-env"],
+//       ["@babel/preset-react", { runtime: "automatic" }],
+//     ],
+//     plugins: [
+//       [
+//         "@babel/plugin-transform-react-jsx",
+//         { runtime: "automatic", throwIfNamespace: false },
+//       ],
+//     ],
+//   },
+// };
--- frontend/src/pages/student/StudentHome.js
+++ frontend/src/pages/student/StudentHome.js
@@ -1,35 +1,47 @@
+import React, { useEffect, useRef, useState } from "react";
 import StudentNavbar from "../../components/Common/StudentNavbar/StudentNavbar";
-// import PageWrapper from "../../components/Common/PageWrapper";
-import { useEffect, useState } from "react";
-import Playlist from "../../components/Sidebar/Playlist";
-// import VideoPlayerWrapper from "../../components/StackVideoShaka/VideoPlayerWrapper";
 import useUserStore from "../../store/UserStore";
 import { Fetch } from "../../utils/Fetch";
-import VideoPlayerWrapper from "./shared-components/StackVideoDashJS/VideoPlayerWrapper";
-// import VideoPlayerWrapper from "../../../../solid-frontend/src/components/StackVideoDashJS/VideoPlayerWrapper";
-// import VideoPlayerWrapper from "../../solid-frontend/src/components/StackVideoDashJS/VideoPlayerWrapper";
+import StudentPageWrapper from "../../components/Common/StudentPageWrapper";
 import {
-  convertToReactComponent,
-  ReactToSolidBridgeProvider,
-} from "react-solid-bridge";
+  PoseLandmarker,
+  FilesetResolver,
+  DrawingUtils,
+} from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
 
 import "./MovingText.css";
+import { Button } from "@geist-ui/core";
 
 function StudentHome() {
-  const [position, setPosition] = useState(0);
-  const ReactMySolidComponent = convertToReactComponent(VideoPlayerWrapper);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPosition((prevPosition) => (prevPosition + 1) % 100);
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  let user = useUserStore((state) => state.user);
   const [userPlan, setUserPlan] = useState({});
   const [planId, setPlanId] = useState(0);
+  const [lastVideoTime, setLastVideoTime] = useState(-1);
+  const [poseLandmarker, setPoseLandmarker] = useState(null);
+  const webcamRunningRef = useRef(false); // Ref to store webcamRunning state
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const enableWebcamButtonRef = useRef(null);
+
+  let user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    const createPoseLandmarker = async () => {
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+      );
+
+      const landmarker = await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+          delegate: "GPU",
+        },
+        runningMode: "IMAGE",
+        numPoses: 2,
+      });
+      setPoseLandmarker(landmarker);
+    };
+    createPoseLandmarker();
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,34 +63,111 @@ function StudentHome() {
     }
   }, [user]);
 
-  return (
-    <div className="flex-col justify-center">
-      <StudentNavbar />
-      {/* <div>Welcome {user.name}!</div> */}
-      <br />
-      <br />
-      <div class="moving-text-bar py-2">
-        <div class="moving-text">
-          This video is for personal self-practice only and not to be used by
-          teachers for taking classes. Any unauthorized use may kindly be
-          notified to: +91-9980802351
-        </div>
-      </div>
+  const startWebcam = () => {
+    const constraints = { video: true };
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      videoRef.current.srcObject = stream;
+      videoRef.current.style.display = "block";
+      videoRef.current.play(); // Ensure the video starts playing
+      videoRef.current.addEventListener("loadeddata", predictWebcam);
+    });
+  };
 
-      <div className="mx-auto max-w-7xl">
-        <div className="my-10">
-          {/* <VideoPlayerWrapper /> */}
-          <ReactToSolidBridgeProvider>
-            <div>
-              <h2>React Component</h2>
-              <ReactMySolidComponent />
-            </div>
-          </ReactToSolidBridgeProvider>
-          <hr />
-          <Playlist />
+  const predictWebcam = async () => {
+    const videoHeight = 360;
+    const videoWidth = 480;
+    const canvasElement = canvasRef.current;
+    const video = videoRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    const drawingUtils = new DrawingUtils(canvasCtx);
+
+    canvasElement.height = videoHeight;
+    canvasElement.width = videoWidth;
+    video.height = videoHeight;
+    video.width = videoWidth;
+    await poseLandmarker.setOptions({ runningMode: "VIDEO" });
+
+    const startTimeMs = performance.now();
+    if (video.currentTime !== lastVideoTime) {
+      console.log("running predict web cam");
+      setLastVideoTime(video.currentTime);
+      poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
+        canvasCtx.save();
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+        for (const landmark of result.landmarks) {
+          drawingUtils.drawLandmarks(landmark, {
+            radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1),
+          });
+          drawingUtils.drawConnectors(
+            landmark,
+            PoseLandmarker.POSE_CONNECTIONS
+          );
+        }
+        canvasCtx.restore();
+      });
+    }
+    console.log(
+      "from predict func, webcam running is : ",
+      webcamRunningRef.current
+    );
+    if (webcamRunningRef.current) {
+      console.log("calling : ", predictWebcam);
+      window.requestAnimationFrame(predictWebcam);
+    }
+  };
+
+  const enableCam = (event) => {
+    if (!poseLandmarker) {
+      return;
+    }
+    if (webcamRunningRef.current) {
+      webcamRunningRef.current = false;
+      enableWebcamButtonRef.current.innerText = "ENABLE PREDICTIONS";
+    } else {
+      webcamRunningRef.current = true;
+      enableWebcamButtonRef.current.innerText = "DISABLE PREDICTIONS";
+      startWebcam();
+    }
+    console.log(
+      "Webcam running (inside enableCam): ",
+      webcamRunningRef.current
+    );
+  };
+
+  return (
+    <StudentPageWrapper heading="Player">
+      <div className="border-8 border-gray-950">
+        <Button ref={enableWebcamButtonRef} onClick={enableCam}>
+          ENABLE PREDICTIONS
+        </Button>
+        <div style={{ position: "relative", width: "480px", height: "360px" }}>
+          <video
+            id="webcam"
+            ref={videoRef}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            autoPlay
+            muted
+          ></video>
+          <canvas
+            id="output_canvas"
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          ></canvas>
         </div>
       </div>
-    </div>
+    </StudentPageWrapper>
   );
 }
 
