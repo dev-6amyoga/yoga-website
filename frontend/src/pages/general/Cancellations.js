35a492e3c45ba04e630307b616d6ab37fa2865f4
smriti0302 <smriti030202@gmail.com>
2024-05-23 16:38:45 +0530

    updates

---
+++ frontend/src/pages/general/Cancellations.js
@@ -0,0 +1,28 @@
+import { Card } from "@geist-ui/core";
+
+function Cancellations() {
+  return (
+    <PageWrapper>
+      <Card width="100%" type="dark">
+        <Text h4 my={0}>
+          Cancellations and Refunds
+        </Text>
+        <Text>
+          Subscription once taken cannot be cancelled under any circumstances.
+        </Text>
+        <Text>
+          Fees once paid will not be refunded under any circumstances.
+        </Text>
+        <Text>
+          Incase of any special circumstances, decisions will be taken on a
+          case-to-case basis depending on the gravity of the situation.
+        </Text>
+        <Text>Decision taken by 6AM will be final.</Text>
+        <Card.Footer>
+          <Text>For any queries contact us at +91990802351.</Text>
+        </Card.Footer>
+      </Card>
+    </PageWrapper>
+  );
+}
+export default Cancellations;
---
+++ frontend/src/pages/general/PlansAndPricing.js
@@ -0,0 +1,35 @@
+function PlansAndPricing() {
+  return (
+    <PageWrapper>
+      <Card width="100%" type="dark">
+        <Text h4 my={0}>
+          Plans and Pricing
+        </Text>
+        <Text>We currently have two plans for students</Text>
+        <Card>
+          <Text>Basic Plan</Text>
+          <Text>Play 6AM Yoga Playlists!</Text>
+          <Text>50 Hours Watch Time Limit</Text>
+          <Text>30 days validity</Text>
+          <Text>INR 1999</Text>
+        </Card>
+
+        <Card>
+          <Text>Basic Plan</Text>
+          <Text>Play 6AM Yoga Playlists!</Text>
+          <Text>50 Hours Watch Time Limit</Text>
+          <Text>90 days validity</Text>
+          <Text>INR 4999</Text>
+        </Card>
+
+        <Card.Footer>
+          <Text>
+            For any queries contact us at +91990802351 or write us at
+            992351@gmail.com
+          </Text>
+        </Card.Footer>
+      </Card>
+    </PageWrapper>
+  );
+}
+export default PlansAndPricing;
