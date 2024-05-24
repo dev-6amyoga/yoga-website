9ae1d57d635b2b18acb512884474793ad29d7983
smriti0302 <smriti030202@gmail.com>
2024-05-24 23:56:36 +0530

    ssl

--- backend/init.sequelize.js
+++ backend/init.sequelize.js
@@ -5,25 +5,30 @@ const SQLite = require("sqlite3");
 dotenv.config();
 
 const DB_DATABASE = process.env.DB_DATABASE,
-	DB_USERNAME = process.env.DB_USERNAME,
-	DB_PASSWORD = process.env.DB_PASSWORD,
-	DB_HOST = process.env.DB_HOST,
-	DB_PORT = process.env.DB_PORT;
+  DB_USERNAME = process.env.DB_USERNAME,
+  DB_PASSWORD = process.env.DB_PASSWORD,
+  DB_HOST = process.env.DB_HOST,
+  DB_PORT = process.env.DB_PORT;
 
 // console.log(DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 var sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-	dialect: "postgres",
-	logging: false,
-	host: DB_HOST,
-	port: DB_PORT,
-	dialectOptions: {},
-	benchmark: true,
+  dialect: "postgres",
+  logging: false,
+  host: DB_HOST,
+  port: DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
+  benchmark: true,
 });
 
 // Initializes and syncs the db
 function initializeSequelize() {
-	return sequelize.sync({ alter: true });
-	// return sequelize.sync({ force: true });
+  //   return sequelize.sync({ alter: true });
+  return sequelize.sync({ force: true });
 }
 
 module.exports = { sequelize, initializeSequelize };
