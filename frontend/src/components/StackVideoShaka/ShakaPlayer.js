32038d080286168671443904948d1ca865529f5e
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-05-31 01:13:51 +0530

    Tryin shaka

--- frontend/src/components/StackVideoShaka/ShakaPlayer.js
+++ frontend/src/components/StackVideoShaka/ShakaPlayer.js
@@ -1,3 +1,4 @@
+import React from "react";
 import shaka from "shaka-player/dist/shaka-player.ui";
 
 /**
---
+++ frontend/src/pages/testing/ShakaVideo.js
@@ -0,0 +1,9 @@
+import VideoPlayerWrapper from "../../components/StackVideoShaka/VideoPlayerWrapper";
+
+export default function ShakaVideo() {
+	return (
+		<div>
+			<VideoPlayerWrapper />
+		</div>
+	);
+}
