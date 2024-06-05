c663b3fad6e7bd66d980229b7b6a729002651c0e
smriti0302 <smriti030202@gmail.com>
2024-06-05 17:40:40 +0530

    upd

--- frontend/src/components/Common/StudentPageWrapper.js
+++ frontend/src/components/Common/StudentPageWrapper.js
@@ -1,17 +1,17 @@
 import StudentNavbar from "./StudentNavbar/StudentNavbar";
-
+import AppAppBar from "../SidebarMui";
 export default function StudentPageWrapper({ heading, children }) {
-	return (
-		<>
-			<StudentNavbar />
-			<div className="max-w-7xl mx-auto py-20 px-4 xl:px-0">
-				{heading ? (
-					<h1 className="pt-4 font-bold text-center">{heading}</h1>
-				) : (
-					<></>
-				)}
-				<div className="mt-6">{children}</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <AppAppBar />
+      <div className="max-w-8xl mx-auto py-20 px-4 xl:px-0">
+        {heading ? (
+          <h1 className="pt-4 font-bold text-center">{heading}</h1>
+        ) : (
+          <></>
+        )}
+        <div className="mt-6">{children}</div>
+      </div>
+    </>
+  );
 }
