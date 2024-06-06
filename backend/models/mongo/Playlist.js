213737f34443938daa79f2e09df24de1dedc6f65
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-06-06 09:52:49 +0530

    Added manifest combiner with r2 uploads

--- backend/models/mongo/Playlist.js
+++ backend/models/mongo/Playlist.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
 const PlaylistSchema6am = new mongoose.Schema({
-  playlist_id: { type: Number, required: true },
-  playlist_name: String,
-  asana_ids: [mongoose.Schema.Types.Mixed],
-  duration: Number,
-  playist_mode: String,
+	playlist_id: { type: Number, required: true },
+	playlist_name: String,
+	asana_ids: [mongoose.Schema.Types.Mixed],
+	playlist_dash_url: String,
+	duration: Number,
+	playist_mode: String,
 });
 
 const Playlist = mongoose.model(
-  "Playlists",
-  PlaylistSchema6am,
-  "6amyoga_playlists"
+	"Playlists",
+	PlaylistSchema6am,
+	"6amyoga_playlists"
 );
 
 module.exports = Playlist;
