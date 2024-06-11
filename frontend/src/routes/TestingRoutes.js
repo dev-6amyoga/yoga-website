d6fa7b75f970e577bf236d9a02147305361432c8
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-06-12 01:42:03 +0530

    Class mode sample page for student

--- frontend/src/routes/TestingRoutes.js
+++ frontend/src/routes/TestingRoutes.js
@@ -1,6 +1,7 @@
 // import withAuth from "../utils/withAuth";
 import AutomaticRegister from "../components/content-management/forms/AutomaticRegister";
 import TeacherPlaylistCreation from "../pages/teacher/TeacherPlaylistCreation";
+import ClassModeStudent from "../pages/testing/ClassModeStudent";
 import DesignBoard from "../pages/testing/DesignBoard";
 import ShakaVideo from "../pages/testing/ShakaVideo";
 import SocketPage from "../pages/testing/Socket";
@@ -31,7 +32,7 @@ export const TestingRoutes = [
 	},
 	{
 		path: "/testing/class-mode-student",
-		element: <div />,
+		element: <ClassModeStudent />,
 	},
 	{
 		path: "/testing/class-mode-teacher",
