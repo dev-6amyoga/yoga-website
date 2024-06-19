cb2dbf3f4b4e2fc694e90318bcbbe9c1824f7529
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-06-20 00:58:39 +0530

    shaka changes

--- frontend/src/pages/testing/ShakaVideo.js
+++ frontend/src/pages/testing/ShakaVideo.js
@@ -1,11 +1,11 @@
 import VideoPlayerWrapper from "../../components/StackVideoShaka/VideoPlayerWrapper";
 
 export default function ShakaVideo() {
-  return (
-    // <PageWrapper heading="Player">
-    <div>
-      <VideoPlayerWrapper />
-    </div>
-    // </PageWrapper>
-  );
+	return (
+		// <PageWrapper heading="Player">
+		<div className="max-w-7xl mx-auto p-4">
+			<VideoPlayerWrapper />
+		</div>
+		// </PageWrapper>
+	);
 }
