6d7a28603fa67663151c2bbd1e4bd639d1668ca8
smriti0302 <smriti030202@gmail.com>
2024-06-20 22:51:32 +0530

    ai exp

--- frontend/src/routes/TestingRoutes.js
+++ frontend/src/routes/TestingRoutes.js
@@ -4,41 +4,46 @@ import TeacherPlaylistCreation from "../pages/teacher/TeacherPlaylistCreation";
 import DesignBoard from "../pages/testing/DesignBoard";
 import ShakaVideo from "../pages/testing/ShakaVideo";
 import SocketPage from "../pages/testing/Socket";
+import StudentHome from "../pages/student/StudentHome";
 export const TestingRoutes = [
-	// {
-	//   path: "/testing/video",
-	//   element: <TestingVideo />,
-	// },
-	{
-		path: "/testing/shaka-video",
-		element: <ShakaVideo />,
-	},
-	{
-		path: "/testing/tp",
-		element: <TeacherPlaylistCreation />,
-	},
-	{
-		path: "/testing/design",
-		element: <DesignBoard />,
-	},
-	{
-		path: "/testing/auto-reg",
-		element: <AutomaticRegister />,
-	},
-	{
-		path: "/testing/socket",
-		element: <SocketPage />,
-	},
-	{
-		path: "/testing/class-mode-student",
-		element: <div />,
-	},
-	{
-		path: "/testing/class-mode-teacher",
-		element: <div />,
-	},
-	// {
-	//   path: "/testing/playbar",
-	//   element: <PlaybarPage />,
-	// },
+  // {
+  //   path: "/testing/video",
+  //   element: <TestingVideo />,
+  // },
+  {
+    path: "/testing/shaka-video",
+    element: <ShakaVideo />,
+  },
+  {
+    path: "/testing/ai-student",
+    element: <StudentHome />,
+  },
+  {
+    path: "/testing/tp",
+    element: <TeacherPlaylistCreation />,
+  },
+  {
+    path: "/testing/design",
+    element: <DesignBoard />,
+  },
+  {
+    path: "/testing/auto-reg",
+    element: <AutomaticRegister />,
+  },
+  {
+    path: "/testing/socket",
+    element: <SocketPage />,
+  },
+  {
+    path: "/testing/class-mode-student",
+    element: <div />,
+  },
+  {
+    path: "/testing/class-mode-teacher",
+    element: <div />,
+  },
+  // {
+  //   path: "/testing/playbar",
+  //   element: <PlaybarPage />,
+  // },
 ];
