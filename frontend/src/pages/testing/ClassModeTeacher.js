6a6f6ff3490a7629d500e3490cd2bef6ae716c72
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-06-22 00:57:53 +0530

    Class mode testing

--- frontend/src/pages/testing/ClassModeTeacher.js
+++ frontend/src/pages/testing/ClassModeTeacher.js
@@ -1,11 +1,12 @@
-import { Button } from "@mui/material";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import StudentNavMUI from "../../components/Common/StudentNavbar/StudentNavMUI";
-import VideoPlayer from "../../components/StackVideoShaka/VideoPlayer";
-import Attendees from "../../components/testing/Attendees";
-import ClassInfoTeacher from "../../components/testing/ClassInfoTeacher";
-import PlaylistSectionsTeacher from "../../components/testing/PlaylistSectionsTeacher";
+// import StudentNavMUI from "../../components/Common/StudentNavbar/StudentNavMUI";
+// import Playlist from "../../components/Sidebar/Playlist";
+// import VideoPlayer from "../../components/StackVideoShaka/VideoPlayer";
+// import Attendees from "../../components/testing/Attendees";
+// import ClassInfoTeacher from "../../components/testing/ClassInfoTeacher";
+// import PlaylistSectionsTeacher from "../../components/testing/PlaylistSectionsTeacher";
+import ClassModePlayer from "../../components/class-mode-player-dashjs/ClassModePlayer";
 import useVideoStore from "../../store/VideoStore";
 import Hero from "../student/components/Hero";
 
@@ -14,10 +15,11 @@ function ClassModeTeacher() {
 	const { class_id } = useParams();
 
 	const [connectionOpen, setConnectionOpen] = useState(false);
-	const [socket, setSocket] = useState(null);
+	// const [socket, setSocket] = useState(null);
 	const [queue, setQueue] = useState([]);
 	const [startConnection, setStartConnection] = useState(false);
 	let intervalTimer = useRef(null);
+	let socket = useRef(null);
 
 	const getSampleTeacherReq = (event_type) => {
 		return JSON.stringify({
@@ -31,72 +33,68 @@ function ClassModeTeacher() {
 		});
 	};
 
+	const handleOpen = useCallback(() => {
+		setConnectionOpen(true);
+		console.log("Connection opened");
+		if (socket.current) {
+			// socket.current.send(
+			// 	JSON.stringify({
+			// 		class_id: class_id,
+			// 		teacher_id: "1",
+			// 	})
+			// );
+		}
+	}, []);
+
+	const handleClose = useCallback(() => {
+		console.log("Connection closed");
+		setConnectionOpen(false);
+		clearInterval(intervalTimer.current);
+	}, []);
+
+	const handleMessage = useCallback((e) => {
+		const data = JSON.parse(e.data);
+		console.log("Message received: ", data);
+		setQueue((prev) => [...prev, data]);
+	}, []);
+
 	// if startConnection is true, create a new WebSocket connection
 	useEffect(() => {
 		if (!startConnection) return;
 
 		const ws = new WebSocket("ws://localhost:4949/teacher/ws");
-		setSocket(ws);
-		setStartConnection(false);
-	}, [startConnection]);
+		socket.current = ws;
 
-	// if socket is not null, add event listeners for open, close, and message
-	useEffect(() => {
-		if (socket === null) return;
-
-		const handleOpen = () => {
-			setConnectionOpen(true);
-			// intervalTimer.current = setInterval(() => {
-			// 	socket.send(
-
-			// 	);
-			// }, 5000);
-		};
-
-		const handleClose = () => {
-			setConnectionOpen(false);
-			clearInterval(intervalTimer.current);
-		};
-
-		const handleMessage = (e) => {
-			const data = JSON.parse(e.data);
-			console.log("Message received: ", data);
-			setQueue((prev) => [...prev, data]);
-		};
-
-		socket.addEventListener("open", handleOpen);
-		socket.addEventListener("close", handleClose);
-		socket.addEventListener("message", handleMessage);
+		socket.current.addEventListener("open", handleOpen);
+		socket.current.addEventListener("close", handleClose);
+		socket.current.addEventListener("message", handleMessage);
 
 		return () => {
-			if (socket) {
-				socket.close();
-				socket.removeEventListener("open", handleOpen);
-				socket.removeEventListener("close", handleClose);
-				socket.removeEventListener("message", handleMessage);
+			if (ws) {
+				ws.close();
 			}
 		};
-	}, [socket]);
+	}, [startConnection]);
 
-	const eventTypes = useMemo(() => {
-		return {
-			queue: [EVENT_QUEUE_PUSH, EVENT_QUEUE_POP, EVENT_QUEUE_CLEAR],
-			controls: [
-				EVENT_CONTROLS_PLAY,
-				EVENT_CONTROLS_PAUSE,
-				EVENT_CONTROLS_NEXT,
-				EVENT_CONTROLS_PREV,
-				EVENT_CONTROLS_SEEK_TO,
-				EVENT_CONTROLS_SEEK_MARKER,
-			],
-			timer: [EVENT_TIMER],
-		};
-	}, []);
+	// const eventTypes = useMemo(() => {
+	// 	return {
+	// 		queue: [EVENT_QUEUE_PUSH, EVENT_QUEUE_POP, EVENT_QUEUE_CLEAR],
+	// 		controls: [
+	// 			EVENT_CONTROLS_PLAY,
+	// 			EVENT_CONTROLS_PAUSE,
+	// 			EVENT_CONTROLS_NEXT,
+	// 			EVENT_CONTROLS_PREV,
+	// 			EVENT_CONTROLS_SEEK_TO,
+	// 			EVENT_CONTROLS_SEEK_MARKER,
+	// 		],
+	// 		timer: [EVENT_TIMER],
+	// 	};
+	// }, []);
 
 	return (
 		<main>
 			<Hero heading="Class Mode" />
-			<StudentNavMUI />
+			{/* <StudentNavMUI />
 			<div className="max-w-7xl mx-auto py-2 px-1 xl:px-0">
 				<div className="mt-6">
 					<>
@@ -105,9 +103,15 @@ function ClassModeTeacher() {
 							variant="contained"
 							color="primary"
 							onClick={() => {
-								setStartConnection(true);
+								if (connectionOpen && socket.current) {
+									setStartConnection(false);
+								} else {
+									setStartConnection(true);
+								}
 							}}>
-							{connectionOpen ? "Connected" : "Not Connected"}
+							{connectionOpen
+								? "Close Connection"
+								: "Open Connection"}
 						</Button>
 
 						<div
@@ -142,9 +146,16 @@ function ClassModeTeacher() {
 						)}
 
 						<Attendees />
+
+						<Spacer h={2} />
+
+						<Playlist />
 					</>
 				</div>
-			</div>
+			</div> */}
+
+			{/* dash video player */}
+			<ClassModePlayer />
 		</main>
 	);
 }
