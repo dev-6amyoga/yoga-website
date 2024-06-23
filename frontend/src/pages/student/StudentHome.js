7c74aba4b92de00dfcb5d449e8ca69cad46d34b1
smriti0302 <smriti030202@gmail.com>
2024-06-23 23:18:09 +0530

    home student

--- frontend/src/pages/student/StudentHome.js
+++ frontend/src/pages/student/StudentHome.js
@@ -170,6 +170,12 @@ function StudentHome() {
             handleScoreUpdate(17.5);
           } else if (globalScore === 38.75) {
             handleScoreUpdate(8.75);
+          } else if (globalScore === 47.5) {
+            // do nothing
+          } else if (globalScore === 56.25) {
+            handleScoreUpdate(-8.75);
+          } else if (globalScore === 65) {
+            handleScoreUpdate(-17.5);
           }
         } else {
           handleScoreUpdate(-17.5);
@@ -185,6 +191,10 @@ function StudentHome() {
               handleScoreUpdate(17.5);
             } else if (globalScore === 47.5) {
               handleScoreUpdate(8.75);
+            } else if (globalScore === 56.25) {
+              //do nothing
+            } else if (globalScore === 65) {
+              handleScoreUpdate(-8.75);
             }
           } else {
             handleScoreUpdate(-8.75);
