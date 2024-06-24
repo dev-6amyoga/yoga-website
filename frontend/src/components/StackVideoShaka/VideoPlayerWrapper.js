32c1210a657325c69167d3b893f67a4e1c116e54
smriti0302 <smriti030202@gmail.com>
2024-06-24 23:06:04 +0530

    upddd

--- frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
+++ frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
@@ -6,36 +6,36 @@ import PlaylistSections from "./Sections";
 import VideoPlayer from "./VideoPlayer";
 
 export default function VideoPlayerWrapper() {
-	const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
+  const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
 
-	return (
-		<>
-			<div
-				className={
-					fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"
-				}>
-				<div
-					className={
-						fullScreen
-							? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
-							: "video-area"
-					}>
-					<VideoPlayer />
-				</div>
-				{!fullScreen ? (
-					<div className="queue-area">
-						<PoseDetector />
-					</div>
-				) : (
-					<></>
-				)}
-			</div>
+  return (
+    <>
+      <div
+        className={fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"}
+      >
+        <div
+          className={
+            fullScreen
+              ? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
+              : "video-area"
+          }
+        >
+          <VideoPlayer />
+        </div>
+        {!fullScreen ? (
+          <div className="queue-area">
+            <PoseDetector />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
 
-			<PlaylistSections />
+      <PlaylistSections />
 
-			<Spacer h={4} />
+      <Spacer h={4} />
 
-			<Playlist />
-		</>
-	);
+      <Playlist />
+    </>
+  );
 }
