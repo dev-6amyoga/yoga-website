1b48694daf1c59556b22a26ffa1c2443ed1e5936
smriti0302 <smriti030202@gmail.com>
2024-06-24 23:45:21 +0530

    pls

--- frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
+++ frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
@@ -1,43 +1,41 @@
+import { Spacer } from "@geist-ui/core";
 import useVideoStore from "../../store/VideoStore";
 import Playlist from "../Sidebar/Playlist";
+import PoseDetector from "../webcam_component/PoseDetector";
 import PlaylistSections from "./Sections";
 import VideoPlayer from "./VideoPlayer";
 
 export default function VideoPlayerWrapper() {
-	const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
+  const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
 
-	return (
-		<>
-			<div
-				className={
-					fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"
-				}>
-				<div
-					className={
-						fullScreen
-							? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
-							: "video-area"
-					}>
-					<VideoPlayer />
-				</div>
-				{!fullScreen ? (
-					<div className="queue-area">
-						<PlaylistSections />
-					</div>
-				) : (
-					<></>
-				)}
-			</div>
+  return (
+    <>
+      <div
+        className={fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"}
+      >
+        <div
+          className={
+            fullScreen
+              ? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
+              : "video-area"
+          }
+        >
+          <VideoPlayer />
+        </div>
+        {!fullScreen ? (
+          <div className="queue-area">
+            <PoseDetector />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
 
-			{fullScreen ? (
-				<div className="queue-area">
-					<PlaylistSections />
-				</div>
-			) : (
-				<></>
-			)}
+      <PlaylistSections />
 
-			<Playlist />
-		</>
-	);
+      <Spacer h={4} />
+
+      <Playlist />
+    </>
+  );
 }
