36e62f772e279dd2189f534a7a5a14bf3a91b5fb
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-02 08:24:03 +0530

    Login updates

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -20,4 +20,71 @@ export class UserAPI {
 			return [null, err];
 		}
 	}
+
+	// check if phone number exists in the database
+	static async postCheckPhoneNumber(phone) {
+		try {
+			const response = await Fetch({
+				url: "/user/check-phone-number",
+				method: "POST",
+				data: {
+					phone: phone,
+				},
+			});
+
+			if (response.status === 200) {
+				return [response.data, null];
+			} else {
+				return [null, response.data];
+			}
+		} catch (err) {
+			return [null, err];
+		}
+	}
+
+	// check if email exists in the database
+	static async postCheckEmail(email) {
+		if (!email) return [false, new Error("Email is required")];
+
+		try {
+			const response = await Fetch({
+				url: "/user/check-email",
+				method: "POST",
+				data: {
+					email: email,
+				},
+			});
+
+			if (response.status === 200) {
+				return [response.data, null];
+			} else {
+				return [null, response.data];
+			}
+		} catch (err) {
+			return [null, err];
+		}
+	}
+
+	// check if username exists in the database
+	static async postCheckUsername(username) {
+		if (!username) return [false, new Error("Username is required")];
+
+		try {
+			const response = await Fetch({
+				url: "/user/check-username",
+				method: "POST",
+				data: {
+					username: username,
+				},
+			});
+
+			if (response.status === 200) {
+				return [response.data, null];
+			} else {
+				return [null, response.data];
+			}
+		} catch (err) {
+			return [null, err];
+		}
+	}
 }
--- frontend/src/utils/formValidation.js
+++ frontend/src/utils/formValidation.js
@@ -1,68 +1,77 @@
-export const validatePhone = (phone) => {
-  if (!phone) return [false, new Error("Phone number is required")];
+export const validatePhone = async (phone) => {
+	if (!phone) return [false, new Error("Phone number is required")];
 
-  const phoneRegex = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+	const phoneRegex = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
-  if (!phoneRegex.test(phone))
-    return [false, new Error("Invalid phone number")];
-  else return [true, null];
+	if (!phoneRegex.test(phone))
+		return [false, new Error("Invalid phone number")];
+	else [true, null];
 };
 
 export const validateEmail = (email) => {
-  if (!email) return [false, new Error("Email is required")];
+	if (!email) return [false, new Error("Email is required")];
 
-  const emailRegex = email.match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
+	const emailRegex = email.match(
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	);
 
-  if (!emailRegex) return [false, new Error("Invalid email address")];
-  else return [true, null];
+	if (!emailRegex) return [false, new Error("Invalid email address")];
+	else return [true, null];
 };
 
 export const validatePassword = (password) => {
-  if (!password) return [false, new Error("Password is required")];
+	if (!password) return [false, new Error("Password is required")];
 
-  if (typeof password === "string" && password.length < 8)
-    return [false, new Error("Password must contain at least 8 characters")];
+	if (typeof password === "string" && password.length < 8)
+		return [
+			false,
+			new Error("Password must contain at least 8 characters"),
+		];
 
-  // lowercase letter check
-  if (!password.match(/^(?=.*[a-z])/)) {
-    return [
-      false,
-      new Error("Password must contain at least 1 lowercase character"),
-    ];
-  }
+	// lowercase letter check
+	if (!password.match(/^(?=.*[a-z])/)) {
+		return [
+			false,
+			new Error("Password must contain at least 1 lowercase character"),
+		];
+	}
 
-  // digit check
-  if (!password.match(/^(?=.*[0-9])/)) {
-    return [
-      false,
-      new Error("Password must contain at least 1 numeric character"),
-    ];
-  }
+	// digit check
+	if (!password.match(/^(?=.*[0-9])/)) {
+		return [
+			false,
+			new Error("Password must contain at least 1 numeric character"),
+		];
+	}
 
-  // special character check
-  if (!password.match(/^(?=.*[!@#$%^&*,?])/)) {
-    return [
-      false,
-      new Error("Password must contain at least 1 special character"),
-    ];
-  } else return [true, null];
+	// special character check
+	if (!password.match(/^(?=.*[!@#$%^&*,?])/)) {
+		return [
+			false,
+			new Error("Password must contain at least 1 special character"),
+		];
+	} else return [true, null];
 };
 
 export const validateUsername = (username) => {
-  if (!username) return [false, new Error("Username is required")];
+	if (!username) return [false, new Error("Username is required")];
 
-  if (typeof username === "string" && username.length < 4)
-    return [false, new Error("Username must contain at least 4 characters")];
+	if (typeof username === "string" && username.length < 4)
+		return [
+			false,
+			new Error("Username must contain at least 4 characters"),
+		];
 
-  if (typeof username === "string" && username.length > 25)
-    return [false, new Error("Username must contain at most 20 characters")];
+	if (typeof username === "string" && username.length > 25)
+		return [
+			false,
+			new Error("Username must contain at most 20 characters"),
+		];
 
-  if (typeof username === "string" && !username.match(/^[a-zA-Z0-9_]+$/))
-    return [
-      false,
-      new Error("Username must contain only alphanumeric characters"),
-    ];
-  else return [true, null];
+	if (typeof username === "string" && !username.match(/^[a-zA-Z0-9_]+$/))
+		return [
+			false,
+			new Error("Username must contain only alphanumeric characters"),
+		];
+	else return [true, null];
 };
