83e0379a93c4fe2043afb0e4a9e36f39d9304201
smriti0302 <smriti030202@gmail.com>
2024-07-03 19:29:13 +0530

    upd1

--- frontend/src/components/auth/register/RoleSelectorForm.js
+++ frontend/src/components/auth/register/RoleSelectorForm.js
@@ -1,24 +1,26 @@
 import { UserCheck } from "@geist-ui/icons";
 
 export default function RoleSelectorForm({ role, setRole, handleNextStep }) {
-	return (
-		<form className="flex flex-col gap-4 w-full" onSubmit={() => {}}>
-			<h6 className="text-center">Select Role</h6>
+  return (
+    <form className="flex flex-col gap-4 w-full" onSubmit={() => {}}>
+      <h6 className="text-center">Select Role</h6>
 
-			<div className="flex gap-4 items-center justify-center">
-				<div
-					className={`flex items-center gap-2 flex-col p-8 border-2 rounded-lg ${
-						role === "STUDENT" ? "border-blue-500" : ""
-					}`}
-					onClick={() => {
-						setRole("STUDENT");
-						handleNextStep();
-					}}>
-					<UserCheck />
-					Student
-				</div>
-				{/* enable for institute */}
-				{/* <div
+      <div className="flex gap-4 items-center justify-center">
+        <div
+          className={`flex items-center gap-2 flex-col p-8 border-2 rounded-lg ${
+            role === "STUDENT" ? "border-blue-500" : ""
+          } cursor-pointer`} // Add cursor-pointer class here
+          onClick={() => {
+            setRole("STUDENT");
+            handleNextStep();
+          }}
+        >
+          <UserCheck />
+          Student
+        </div>
+
+        {/* enable for institute */}
+        {/* <div
           className={`flex items-center gap-2 flex-col p-8 border rounded-lg ${
             role === "INSTITUTE_OWNER" ? "border-blue-500" : ""
           }`}
@@ -30,7 +32,7 @@ export default function RoleSelectorForm({ role, setRole, handleNextStep }) {
           <Briefcase />
           Institute
         </div> */}
-			</div>
-		</form>
-	);
+      </div>
+    </form>
+  );
 }
