7e0f95060ca874ab319a779e1a6c51b01d4d2531
smriti0302 <smriti030202@gmail.com>
2024-07-04 21:37:09 +0530

    upd11

--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -2,22 +2,26 @@ import Auth from "../pages/auth/Auth";
 import EmailVerification from "../pages/email-verify/EmailVerification";
 import Otp from "../pages/otp/Otp";
 import UpdateScreen from "../pages/student/UpdateScreen";
-
+import ForgotPassword from "../components/forgot-password/ForgotPassword";
 export const AuthRoutes = [
-	{
-		path: "/auth",
-		element: <Auth />,
-	},
-	{
-		path: "/auth/otp-phone",
-		element: <Otp />,
-	},
-	{
-		path: "auth/verify-email",
-		element: <EmailVerification />,
-	},
-	{
-		path: "auth/update-email",
-		element: <UpdateScreen />,
-	},
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+  {
+    path: "/auth/otp-phone",
+    element: <Otp />,
+  },
+  {
+    path: "auth/verify-email",
+    element: <EmailVerification />,
+  },
+  {
+    path: "/user/forgotPassword",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "auth/update-email",
+    element: <UpdateScreen />,
+  },
 ];
