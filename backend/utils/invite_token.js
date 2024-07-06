2519b2c5f5a4ab0646deaeca75f5771cdf6e040d
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-06 17:16:30 +0530

    Forgot pass

--- backend/utils/invite_token.js
+++ backend/utils/invite_token.js
@@ -1,24 +1,27 @@
 const crypto = require("crypto-js");
 
 module.exports = {
-  enc_token: (invite_user_email, invite_user_phone, invite_user_name) => {
-    const bytes = crypto.RC4.encrypt(
-      JSON.stringify({
-        invite_user_email,
-        invite_user_phone,
-        invite_user_name,
-      }),
-      "secret"
-    );
+	enc_token: (invite_user_email, invite_user_phone, invite_user_name) => {
+		const bytes = crypto.RC4.encrypt(
+			JSON.stringify({
+				invite_user_email,
+				invite_user_phone,
+				invite_user_name,
+				create_time: new Date().toISOString(),
+			}),
+			"secret"
+		);
 
-    const token = bytes.toString();
-    return token;
-  },
-  dec_token: (token) => {
-    const bytes = crypto.RC4.decrypt(token, "secret");
-
-    const invite = JSON.parse(bytes.toString(crypto.enc.Utf8));
-
-    return invite;
-  },
+		const token = bytes.toString();
+		return token;
+	},
+	dec_token: (token) => {
+		try {
+			const bytes = crypto.RC4.decrypt(token, "secret");
+			const invite = JSON.parse(bytes.toString(crypto.enc.Utf8));
+			return invite;
+		} catch (err) {
+			return null;
+		}
+	},
 };
