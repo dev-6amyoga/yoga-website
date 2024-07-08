15c1158718384b3af53e774501ac756415518af0
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-08 22:44:16 +0530

    Invoice download to pdf

--- syncer-backend/src/server/service.go
+++ syncer-backend/src/server/service.go
@@ -1,54 +1,54 @@
 package server
 
 import (
-	// "syncer-backend/src/class"
+	"syncer-backend/src/class"
 	"syncer-backend/src/events"
 
 	"github.com/gorilla/websocket"
 )
 
 func (s *Server) ProcessQueueEvent(classId string, queueEvent events.QueueEvent, conn *websocket.Conn) {
-	// switch queueEvent.SubType {
-	// case events.EVENT_QUEUE_PUSH:
-	// 	_, ok := (queueEvent.Data).(events.QueueEventPushData)
+	switch queueEvent.SubType {
+	case events.EVENT_QUEUE_PUSH:
+		_, ok := (queueEvent.Data).(events.QueueEventPushData)
 
-	// 	if !ok {
-	// 		conn.WriteJSON(events.EventTeacherResponse{
-	// 			Status:  events.EVENT_STATUS_NACK,
-	// 			Message: "Error unmarshalling queue push data",
-	// 		})
-	// 		s.logger.Error("Error type casting push data")
-	// 		return
-	// 	}
+		if !ok {
+			conn.WriteJSON(events.EventTeacherResponse{
+				Status:  events.EVENT_STATUS_NACK,
+				Message: "Error unmarshalling queue push data",
+			})
+			s.logger.Error("Error type casting push data")
+			return
+		}
 
-	// 	// push to the db
-	// 	class.AddToActionsQueue(s.dbClient, classId, queueEvent)
+		// push to the db
+		class.AddToActionsQueue(s.dbClient, classId, queueEvent)
 
-	// case events.EVENT_QUEUE_POP:
-	// 	_, ok := (queueEvent.Data).(events.QueueEventPopData)
+	case events.EVENT_QUEUE_POP:
+		_, ok := (queueEvent.Data).(events.QueueEventPopData)
 
-	// 	if !ok {
-	// 		conn.WriteJSON(events.EventTeacherResponse{
-	// 			Status:  events.EVENT_STATUS_NACK,
-	// 			Message: "Error unmarshalling queue push data",
-	// 		})
-	// 		s.logger.Error("Error unmarshalling message:")
-	// 		return
-	// 	}
+		if !ok {
+			conn.WriteJSON(events.EventTeacherResponse{
+				Status:  events.EVENT_STATUS_NACK,
+				Message: "Error unmarshalling queue push data",
+			})
+			s.logger.Error("Error unmarshalling message:")
+			return
+		}
 
-	// 	// push to the db
-	// 	class.AddToActionsQueue(s.dbClient, classId, queueEvent)
+		// push to the db
+		class.AddToActionsQueue(s.dbClient, classId, queueEvent)
 
-	// case events.EVENT_QUEUE_CLEAR:
-	// 	class.AddToActionsQueue(s.dbClient, classId, queueEvent)
+	case events.EVENT_QUEUE_CLEAR:
+		class.AddToActionsQueue(s.dbClient, classId, queueEvent)
 
-	// default:
-	// 	conn.WriteJSON(events.EventTeacherResponse{
-	// 		Status:  events.EVENT_STATUS_NACK,
-	// 		Message: "Invalid queue event subtype",
-	// 	})
+	default:
+		conn.WriteJSON(events.EventTeacherResponse{
+			Status:  events.EVENT_STATUS_NACK,
+			Message: "Invalid queue event subtype",
+		})
 
-	// }
+	}
 }
 
 func (s *Server) ProcessControlsEvent(classId string, controlsEvent events.ControlsEvent, conn *websocket.Conn) {
