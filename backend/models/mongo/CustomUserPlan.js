6c2c95cb5bbf73a75126ef0d946c773bb0bb8bb3
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-11 00:34:50 +0530

    Updates for custom plan

--- backend/models/mongo/CustomUserPlan.js
+++ backend/models/mongo/CustomUserPlan.js
@@ -1,21 +1,29 @@
 const mongoose = require("mongoose");
 
 const customUserPlanSchema = new mongoose.Schema({
-  custom_user_plan_id: Number,
-  purchase_date: Date,
-  validity_from: Date,
-  validity_to: Date,
-  transaction_order_id: String,
-  current_status: String,
-  auto_renewal_enabled: Boolean,
-  user_id: Number,
-  plan_id: Number,
+	purchase_date: Date,
+	validity_from: Date,
+	validity_to: Date,
+	transaction_order_id: String,
+	current_status: String,
+	auto_renewal_enabled: Boolean,
+	user_id: Number,
+	custom_plan_id: Number,
+	user_type: String,
+	created: {
+		type: mongoose.Schema.Types.Date,
+		default: Date.now,
+	},
+	updated: {
+		type: mongoose.Schema.Types.Date,
+		default: Date.now,
+	},
 });
 
 const CustomUserPlan = mongoose.model(
-  "CustomUserPlan",
-  customUserPlanSchema,
-  "custom_user_plan"
+	"CustomUserPlan",
+	customUserPlanSchema,
+	"custom_user_plan"
 );
 
 module.exports = CustomUserPlan;
--- backend/routes/CustomUserPlan.js
+++ backend/routes/CustomUserPlan.js
@@ -2,54 +2,156 @@ const express = require("express");
 const router = express.Router();
 const CustomUserPlan = require("../models/mongo/CustomUserPlan");
 const {
-  HTTP_OK,
-  HTTP_NOT_FOUND,
-  HTTP_INTERNAL_SERVER_ERROR,
+	HTTP_OK,
+	HTTP_NOT_FOUND,
+	HTTP_INTERNAL_SERVER_ERROR,
 } = require("../utils/http_status_codes");
+const { authenticateToken } = require("../utils/jwt");
+const CustomPlan = require("../models/mongo/CustomPlan");
 
-router.post("/addCustomUserPlan", async (req, res) => {
-  try {
-    const requestData = req.body;
-    const maxIdCustomPlan = await CustomUserPlan.findOne(
-      {},
-      {},
-      { sort: { custom_user_plan_id: -1 } }
-    );
-    const newCustomPlanId = maxIdCustomPlan
-      ? maxIdCustomPlan.custom_user_plan_id + 1
-      : 1;
+router.post("/addCustomUserPlan", authenticateToken, async (req, res) => {
+	try {
+		const {
+			plan_id,
+			institute_id,
 
-    requestData.custom_user_plan_id = newCustomPlanId;
-    const newCustomPlan = new CustomUserPlan(requestData);
-    const saveCustomPlan = await newCustomPlan.save();
-    res.status(HTTP_OK).json(saveCustomPlan);
-  } catch (error) {
-    console.error("Error saving new custom user plan:", error);
-    res.status(HTTP_INTERNAL_SERVER_ERROR).json({
-      error: "Failed to save new custom user plan",
-    });
-  }
+			purchase_date,
+			validity_from = null,
+			validity_to = null,
+
+			// cancellation_date,
+			auto_renewal_enabled = false,
+
+			discount_coupon_id,
+			referral_code_id,
+
+			current_status,
+			transaction_order_id,
+
+			user_type,
+		} = req.body;
+
+		const { user_id } = req.user;
+
+		// check request body
+
+		if (
+			user_id === null ||
+			user_id === undefined ||
+			!plan_id ||
+			institute_id === undefined ||
+			!purchase_date ||
+			!current_status ||
+			!transaction_order_id ||
+			!user_type
+		) {
+			return res
+				.status(HTTP_BAD_REQUEST)
+				.json({ message: "Missing required fields" });
+		}
+
+		// mongodb object id is string
+		if (typeof plan_id !== "string") {
+			return res.status(HTTP_BAD_REQUEST).json({
+				message: "Invalid plan_id",
+			});
+		}
+
+		if (
+			current_status === USER_PLAN_ACTIVE &&
+			(validity_from === null ||
+				validity_from === undefined ||
+				validity_to === null ||
+				validity_to === undefined)
+		) {
+			// if status is active and either valid from or valid to are null;
+			return res
+				.status(HTTP_BAD_REQUEST)
+				.json({ error: "Missing required fields" });
+		} else if (current_status === USER_PLAN_STAGED) {
+		} else {
+			console.log("INVALID STATUS", current_status);
+			if (
+				current_status !== USER_PLAN_ACTIVE &&
+				current_status !== USER_PLAN_STAGED
+			) {
+				return res
+					.status(HTTP_BAD_REQUEST)
+					.json({ error: "Invalid status" });
+			}
+		}
+
+		// check if plan exists with same status
+
+		if (current_status === USER_PLAN_ACTIVE) {
+			const existingPlan = await CustomUserPlan.findOne({
+				user_id,
+				plan_id,
+				current_status,
+			});
+
+			if (existingPlan) {
+				return res
+					.status(HTTP_BAD_REQUEST)
+					.json({ error: "Plan already exists with same status" });
+			}
+		}
+
+		// check if plan exists
+		const planExists = await CustomPlan.findOne({
+			_id: plan_id,
+		});
+
+		if (!planExists) {
+			return res.status(HTTP_BAD_REQUEST).json({
+				error: "Plan does not exist",
+			});
+		}
+
+		// create new custom user plan
+		const newCustomPlan = new CustomUserPlan({
+			custom_plan_id,
+			user_id,
+			purchase_date,
+			validity_from,
+			validity_to,
+			transaction_order_id,
+			current_status,
+			auto_renewal_enabled,
+			user_type,
+		});
+
+		const saveCustomPlan = await newCustomPlan.save();
+
+		res.status(HTTP_OK).json(saveCustomPlan);
+	} catch (error) {
+		console.error("Error saving new custom user plan:", error);
+		res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+			error: "Failed to save new custom user plan",
+		});
+	}
 });
 
 router.get("/getCustomUserPlansByUser/:user_id", async (req, res) => {
-  try {
-    const userId = parseInt(req.params.user_id);
-    const customUserPlans = await CustomUserPlan.find({ user_id: userId });
-    if (customUserPlans.length > 0) {
-      res
-        .status(HTTP_OK)
-        .json({ plans: customPlans, message: "Custom plans for user exist" });
-    } else {
-      res.status(HTTP_OK).json({
-        message: "No custom plans found for the given user ID",
-      });
-    }
-  } catch (error) {
-    console.error("Error fetching custom plans for user:", error);
-    res.status(HTTP_INTERNAL_SERVER_ERROR).json({
-      error: "Failed to fetch custom plans for user",
-    });
-  }
+	try {
+		const userId = parseInt(req.params.user_id);
+		const customUserPlans = await CustomUserPlan.find({ user_id: userId });
+		if (customUserPlans.length > 0) {
+			res.status(HTTP_OK).json({
+				plans: customPlans,
+				message: "Custom plans for user exist",
+			});
+		} else {
+			res.status(HTTP_OK).json({
+				message: "No custom plans found for the given user ID",
+			});
+		}
+	} catch (error) {
+		console.error("Error fetching custom plans for user:", error);
+		res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+			error: "Failed to fetch custom plans for user",
+		});
+	}
 });
 
 // router.put("/editCustomPlan/:id", async (req, res) => {
