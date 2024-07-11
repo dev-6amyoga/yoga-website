c1d4fa757ba2bce6763480f96e9c84a362b6f9f7
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-11 23:46:16 +0530

    Player UI updates

--- frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
+++ frontend/src/components/StackVideoShaka/VideoPlayerWrapper.js
@@ -1,38 +1,34 @@
-import { Spacer } from "@geist-ui/core";
+import { useState } from "react";
 import useVideoStore from "../../store/VideoStore";
 import Playlist from "../Sidebar/Playlist";
-import PoseDetector from "../webcam_component/PoseDetector";
 import PlaylistSections from "./Sections";
 import VideoPlayer from "./VideoPlayer";
-import { Button } from "@mui/material";
-import { useState } from "react";
 
 export default function VideoPlayerWrapper() {
-  const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
-  const [aiVisible, setAiVisible] = useState(false);
-  return (
-    <>
-      <div
-        className={fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"}
-      >
-        <div
-          className={
-            fullScreen
-              ? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
-              : "video-area"
-          }
-        >
-          <VideoPlayer />
-          <Button
-            onClick={() => {
-              setAiVisible(!aiVisible);
-            }}
-            variant="contained"
-          >
-            {aiVisible ? "Disable AI Mode" : "Enable AI Mode"}
-          </Button>
-        </div>
-        {/* {!fullScreen ? (
+	const [fullScreen] = useVideoStore((state) => [state.fullScreen]);
+	const [aiVisible, setAiVisible] = useState(false);
+	return (
+		<>
+			<div
+				className={
+					fullScreen ? "" : "relative video-grid mb-12 w-full gap-6"
+				}>
+				<div
+					className={
+						fullScreen
+							? "absolute w-full h-screen top-0 left-0 right-0 bottom-0 z-[10000]"
+							: "video-area"
+					}>
+					<VideoPlayer />
+					{/* <Button
+						onClick={() => {
+							setAiVisible(!aiVisible);
+						}}
+						variant="contained">
+						{aiVisible ? "Disable AI Mode" : "Enable AI Mode"}
+					</Button> */}
+				</div>
+				{/* {!fullScreen ? (
           // {aiVisible?  (
           //             <div className="queue-area">
           //   <PoseDetector />
@@ -44,19 +40,20 @@ export default function VideoPlayerWrapper() {
         ) : (
           <></>
         )} */}
+				<div className="queue-area">
+					<PlaylistSections />
+				</div>
 
-        {aiVisible && (
+				{/* {aiVisible && (
           <div className="queue-area">
             <PoseDetector />
           </div>
-        )}
-      </div>
+        )} */}
+			</div>
 
-      <PlaylistSections />
+			{/* <Spacer h={4} /> */}
 
-      <Spacer h={4} />
-
-      <Playlist />
-    </>
-  );
+			<Playlist />
+		</>
+	);
 }
