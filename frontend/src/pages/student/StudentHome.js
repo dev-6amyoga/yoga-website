6436eb9e7f854593bf2e241580fe65ed6542e255
smriti0302 <smriti030202@gmail.com>
2024-07-11 18:55:36 +0530

    ooo

--- frontend/src/pages/student/StudentHome.js
+++ frontend/src/pages/student/StudentHome.js
@@ -10,52 +10,52 @@ import "./MovingText.css";
 import Hero from "./components/Hero";
 
 function StudentHome() {
-	const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
 
-	const defaultTheme = createTheme({ palette: { mode } });
+  const defaultTheme = createTheme({ palette: { mode } });
 
-	const [user, userPlan] = useUserStore((state) => [
-		state.user,
-		state.userPlan,
-	]);
+  const [user, userPlan] = useUserStore((state) => [
+    state.user,
+    state.userPlan,
+  ]);
 
-	// check if user has plan
-	const [hasPlan, setHasPlan] = useState(false);
+  // check if user has plan
+  const [hasPlan, setHasPlan] = useState(false);
 
-	useEffect(() => {
-		if (
-			user !== null &&
-			user !== undefined &&
-			userPlan !== null &&
-			userPlan !== undefined
-		) {
-			// console.log(user, userPlan);
-			if (userPlan.current_status === "ACTIVE") {
-				if (userPlan?.plan?.has_basic_playlist) {
-					setHasPlan(true);
-					return;
-				}
-			}
-			setHasPlan(false);
-		}
-	}, [user, userPlan]);
+  useEffect(() => {
+    if (
+      user !== null &&
+      user !== undefined &&
+      userPlan !== null &&
+      userPlan !== undefined
+    ) {
+      // console.log(user, userPlan);
+      if (userPlan.current_status === "ACTIVE") {
+        if (userPlan?.plan?.has_basic_playlist) {
+          setHasPlan(true);
+          return;
+        }
+      }
+      setHasPlan(false);
+    }
+  }, [user, userPlan]);
 
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<CssBaseline />
-			<StudentNavMUI />
-			<Hero heading="6AM Yoga Player" />
-			{hasPlan ? (
-				<div className="max-w-7xl mx-auto">
-					<VideoPlayerWrapper />
-				</div>
-			) : (
-				<div className="max-w-7xl mx-auto flex items-center text-center justify-center">
-					<h3>Please purchase a plan to view this page.</h3>
-				</div>
-			)}
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <StudentNavMUI />
+      <Hero heading="6AM Yoga Player" />
+      {hasPlan ? (
+        <div className="max-w-7xl mx-auto">
+          <VideoPlayerWrapper />
+        </div>
+      ) : (
+        <div className="max-w-7xl mx-auto flex items-center text-center justify-center">
+          <h3>Please purchase a plan to view this page.</h3>
+        </div>
+      )}
+    </ThemeProvider>
+  );
 }
 
 export default withAuth(StudentHome, ROLE_STUDENT);
