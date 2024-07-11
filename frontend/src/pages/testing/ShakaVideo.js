0d03ce70412bb642224ff967c0465a7157151ced
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-12 01:39:56 +0530

    enalbe 6amplaylistfor testig

--- frontend/src/pages/testing/ShakaVideo.js
+++ frontend/src/pages/testing/ShakaVideo.js
@@ -4,7 +4,7 @@ export default function ShakaVideo() {
 	return (
 		// <PageWrapper heading="Player">
 		<div className="max-w-7xl mx-auto p-4">
-			<VideoPlayerWrapper />
+			<VideoPlayerWrapper page="testing" />
 		</div>
 		// </PageWrapper>
 	);
