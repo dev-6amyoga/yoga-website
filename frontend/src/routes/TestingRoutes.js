d94d1398463f5a6ef45d23bc41d83a7fbb4012c1
smriti0302 <smriti030202@gmail.com>
2024-07-15 16:31:19 +0530

    msg

--- frontend/src/routes/TestingRoutes.js
+++ frontend/src/routes/TestingRoutes.js
@@ -11,56 +11,56 @@ import DesignBoard from "../pages/testing/DesignBoard";
 import ShakaVideo from "../pages/testing/ShakaVideo";
 import SocketPage from "../pages/testing/Socket";
 export const TestingRoutes = [
-	// {
-	//   path: "/testing/video",
-	//   element: <TestingVideo />,
-	// },
-	{
-		path: "/testing/shaka-video",
-		element: <ShakaVideo />,
-	},
-	{
-		path: "/testing/ai-student",
-		element: <StudentHome />,
-	},
-	{
-		path: "/testing/tp",
-		element: <TeacherPlaylistCreation />,
-	},
-	{
-		path: "/testing/design",
-		element: <DesignBoard />,
-	},
-	{
-		path: "/testing/auto-reg",
-		element: <AutomaticRegister />,
-	},
-	{
-		path: "/testing/socket",
-		element: <SocketPage />,
-	},
-	{
-		path: "/testing/class/student/:class_id",
-		element: <ClassModeStudent />,
-	},
-	{
-		path: "/testing/class/teacher/:class_id",
-		element: <ClassModeTeacher />,
-	},
-	{
-		path: "/testing/class/teacher/:class_id/info",
-		element: <ClassInfoTeacher />,
-	},
-	{
-		path: "/testing/class/student/:class_id/info",
-		element: <ClassInfoStudent />,
-	},
-	{
-		path: "/testing/class/manage",
-		element: <ManageClasses />,
-	},
-	// {
-	//   path: "/testing/playbar",
-	//   element: <PlaybarPage />,
-	// },
+  // {
+  //   path: "/testing/video",
+  //   element: <TestingVideo />,
+  // },
+  {
+    path: "/testing/shaka-video",
+    element: <ShakaVideo />,
+  },
+  {
+    path: "/testing/ai-student",
+    element: <StudentHome />,
+  },
+  {
+    path: "/testing/tp",
+    element: <TeacherPlaylistCreation />,
+  },
+  {
+    path: "/testing/design",
+    element: <DesignBoard />,
+  },
+  {
+    path: "/testing/auto-reg",
+    element: <AutomaticRegister />,
+  },
+  {
+    path: "/testing/socket",
+    element: <SocketPage />,
+  },
+  {
+    path: "/class/student/:class_id",
+    element: <ClassModeStudent />,
+  },
+  {
+    path: "/class/teacher/:class_id",
+    element: <ClassModeTeacher />,
+  },
+  {
+    path: "/class/teacher/:class_id/info",
+    element: <ClassInfoTeacher />,
+  },
+  {
+    path: "/class/student/:class_id/info",
+    element: <ClassInfoStudent />,
+  },
+  {
+    path: "/class/manage",
+    element: <ManageClasses />,
+  },
+  // {
+  //   path: "/testing/playbar",
+  //   element: <PlaybarPage />,
+  // },
 ];
