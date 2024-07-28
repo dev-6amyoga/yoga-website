97eeec2364b0ce38be0b9da639567ac558fdecb8
smriti0302 <smriti030202@gmail.com>
2024-07-28 15:59:30 +0530

    po

--- backend/routes/UploadToR2.js
+++ backend/routes/UploadToR2.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { cloudflareAddFileToBucket } = require('../utils/R2Client')
-
+const { cloudflareGetFile } = require('../utils/R2Client')
+const { cloudflareListDir } = require('../utils/R2Client')
 const router = express.Router()
 
 const multer = require('multer')
@@ -32,6 +33,39 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     console.error(err)
     return res.status(500).json({
       message: 'File upload failed',
+    })
+  }
+})
+
+router.get('/videos', async (req, res) => {
+  try {
+    const prefix = ''
+    const { contents, nextContinuationToken } = await cloudflareListDir(prefix)
+    console.log(contents)
+    return res.status(200).json({
+      message: 'Files fetched successfully',
+      data: contents,
+      nextContinuationToken,
+    })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      message: 'Failed to fetch files',
+    })
+  }
+})
+
+router.get('/videos/:filename', async (req, res) => {
+  try {
+    const { filename } = req.params
+    const fileStream = await cloudflareGetFile(filename)
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`)
+    res.setHeader('Content-Type', 'video/mp4')
+    res.send(fileStream)
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      message: 'Failed to download file',
     })
   }
 })
