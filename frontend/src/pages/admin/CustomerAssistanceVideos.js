fdcfe4c57d035baccfad6b3156c6ffd33bb85dda
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-07-30 15:10:28 +0530

    Merge

--- frontend/src/pages/admin/CustomerAssistanceVideos.js
+++ frontend/src/pages/admin/CustomerAssistanceVideos.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import AdminPageWrapper from "../../components/Common/AdminPageWrapper";
@@ -77,118 +77,116 @@ export default function CustomerAssistanceVideos() {
 		},
 	});
 
-	const { isLoading: isDownloadLoading, mutateAsync: handleDownload } =
-		useMutation({
-			mutationKey: ["downloadVideo"],
-			mutationFn: async (video) => {
-				try {
-					let intermediateStart = performance.now();
-					const res = await fetch(
-						`${getBackendDomain()}/r2/videos/${video.Key}`,
-						{
-							method: "GET",
-						}
-					);
-
-					const videoData = await res.arrayBuffer();
-
-					console.log(
-						"res?.data?",
-						videoData
-						// Buffer.from(res.data)
-					);
-
-					console.log(
-						"[download] Time to fetch video: ",
-						performance.now() - intermediateStart
-					);
-
-					// do ffmpeg stuff here
-					intermediateStart = performance.now();
-					const f = new File([videoData], video.Key, {
-						type: "video/mp4",
-					});
-
-					const fileBuffer = await f.stream().getReader().read();
-
-					// if (!fileBuffer.done) {
-					// 	throw new Error("Failed to read file");
-					// }
-
-					// console.log(fileBuffer.value);
-
-					console.log(
-						"[download] Time to create file: ",
-						performance.now() - intermediateStart,
-						fileBuffer.value
-					);
-
-					intermediateStart = performance.now();
-					// do ffmpeg stuff here
-					await ffmpegRef.current.writeFile(
-						"video.mp4",
-						fileBuffer.value
-					);
-
-					console.log(
-						"[download] Time to write file: ",
-						performance.now() - intermediateStart
-					);
-
-					intermediateStart = performance.now();
-					let ffmpegExecResult = await ffmpegRef.current.exec([
-						"-i",
-						"video.mp4",
-						"-sn",
-						"-c:v",
-						"libx264",
-						"-c:a",
-						"aac",
-						"-strict",
-						"experimental",
-						"output.mp4",
-					]);
-
-					console.log(
-						"[download] Time to exec: ",
-						performance.now() - intermediateStart
-					);
-
-					if (ffmpegExecResult !== 0) {
-						throw new Error("FFMPEG failed to execute");
+	const { mutateAsync: handleDownload, isPending } = useMutation({
+		mutationKey: ["downloadVideo"],
+		mutationFn: async (video) => {
+			try {
+				let intermediateStart = performance.now();
+				const res = await fetch(
+					`${getBackendDomain()}/r2/videos/${video.Key}`,
+					{
+						method: "GET",
 					}
+				);
 
-					intermediateStart = performance.now();
+				const videoData = await res.arrayBuffer();
 
-					const data = await ffmpegRef.current.readFile("output.mp4");
+				console.log(
+					"res?.data?",
+					videoData
+					// Buffer.from(res.data)
+				);
 
-					// videoBlobFile = new File(data, "output.mp4", {
-					// 	type: "video/mp4",
-					// });
+				console.log(
+					"[download] Time to fetch video: ",
+					performance.now() - intermediateStart
+				);
 
-					console.log(
-						"[download] Time to read file, parse: ",
-						performance.now() - intermediateStart
-					);
+				// do ffmpeg stuff here
+				intermediateStart = performance.now();
+				const f = new File([videoData], video.Key, {
+					type: "video/mp4",
+				});
 
-					const url = window.URL.createObjectURL(new Blob([data]));
-					const link = document.createElement("a");
-					link.href = url;
-					link.setAttribute("download", `${video.Key}`);
-					document.body.appendChild(link);
-					link.click();
-					link.parentNode.removeChild(link);
-					window.URL.revokeObjectURL(url);
+				const fileBuffer = await f.stream().getReader().read();
 
-					return res.data;
-				} catch (error) {
-					console.error("res?.data?", error);
-					toast.error("Failed to download video");
+				// if (!fileBuffer.done) {
+				// 	throw new Error("Failed to read file");
+				// }
 
-					throw error;
+				// console.log(fileBuffer.value);
+
+				console.log(
+					"[download] Time to create file: ",
+					performance.now() - intermediateStart
+				);
+
+				intermediateStart = performance.now();
+				// do ffmpeg stuff here
+				await ffmpegRef.current.writeFile(
+					"video.mp4",
+					fileBuffer.value
+				);
+
+				console.log(
+					"[download] Time to write file: ",
+					performance.now() - intermediateStart
+				);
+
+				intermediateStart = performance.now();
+				let ffmpegExecResult = await ffmpegRef.current.exec([
+					"-i",
+					"video.mp4",
+					"-sn",
+					"-c:v",
+					"libx264",
+					"-c:a",
+					"aac",
+					"-strict",
+					"experimental",
+					"output.mp4",
+				]);
+
+				console.log(
+					"[download] Time to exec: ",
+					performance.now() - intermediateStart
+				);
+
+				if (ffmpegExecResult !== 0) {
+					throw new Error("FFMPEG failed to execute");
 				}
-			},
-		});
+
+				intermediateStart = performance.now();
+
+				const data = await ffmpegRef.current.readFile("output.mp4");
+
+				// videoBlobFile = new File(data, "output.mp4", {
+				// 	type: "video/mp4",
+				// });
+
+				console.log(
+					"[download] Time to read file, parse: ",
+					performance.now() - intermediateStart
+				);
+
+				const url = window.URL.createObjectURL(new Blob([data]));
+				const link = document.createElement("a");
+				link.href = url;
+				link.setAttribute("download", `${video.Key}`);
+				document.body.appendChild(link);
+				link.click();
+				link.parentNode.removeChild(link);
+				window.URL.revokeObjectURL(url);
+
+				return res.data;
+			} catch (error) {
+				console.error("res?.data?", error);
+				toast.error("Failed to download video");
+
+				throw error;
+			}
+		},
+	});
 
 	const handleDelete = (row) => {};
 
@@ -201,14 +199,22 @@ export default function CustomerAssistanceVideos() {
 			header: "Actions",
 			cell: ({ row }) => {
 				console.log(row?.original);
+				let [loading, setLoading] = useState(false);
+
+				const handle = async (row) => {
+					setLoading(true);
+					await handleDownload(row);
+					setLoading(false);
+				};
+
 				return (
 					<div className="flex flex-row gap-2">
 						<Button
-							onClick={() => handleDownload(row?.original)}
+							onClick={() => handle(row?.original)}
 							variant="contained"
 							size="small"
-							disabled={isDownloadLoading}>
-							Download
+							disabled={loading}>
+							{loading ? <CircularProgress /> : "Download"}
 						</Button>
 						<Button
 							onClick={() => handleDelete(row?.original)}
