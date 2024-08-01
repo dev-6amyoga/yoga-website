ead56ff230105a1bf317477dbe2c06aea60378e6
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-08-01 11:35:39 +0530

    Updates to uploading video

--- backend/demo.py
+++ backend/demo.py
@@ -1,33 +1,41 @@
 import sys
 import gzip
-# import io
+
+import io
+
 # import cloudflare
 import json
 
 import boto3
+
 config = json.load(open("config.json"))
 
 
 s3_client = boto3.client(
-    service_name ="s3",
-    endpoint_url = config["r2"]["endpoint"],
-    aws_access_key_id = config["r2"]["secret_access_key"],
-    aws_secret_access_key = config["r2"]["access_key"],
+    service_name="s3",
+    endpoint_url=config["r2"]["endpoint"],
+    aws_access_key_id=config["r2"]["access_key_id"],
+    aws_secret_access_key=config["r2"]["secret_access_key"],
     region_name="apac",
 )
 
 
-def upload_file(inp_file: str, bucket_dir: str) -> bool:
+def upload_to_r2(filename, file_data, bucket_dir: str) -> bool:
     # if inp_file == None or bucket_dir == None:
     #     return "Invalid input. Exiting."
-    
-    if inp_file == "" or bucket_dir == "":
-        return "Invalid input. Exiting."
-        
-    print("Uploading to R2")
+    print("Uploading to R2", filename)
 
-    s3_client.upload_file(inp_file, bucket_dir, inp_file)
+    try:
 
+        if file_data is None:
+            return "Invalid input. Exiting."
+
+        print("Uploading to R2")
+
+        s3_client.upload_fileobj(io.BytesIO(file_data), bucket_dir, filename)
+
+    except Exception as e:
+        raise e
 
     # p = subprocess.run(["rclone", "copy", {inp_file}, f"r2:{R2_BUCKET}/{bucket_dir}"])
 
@@ -39,19 +47,21 @@ def upload_file(inp_file: str, bucket_dir: str) -> bool:
     # return p.returncode == 0
 
 
-
-def upload_to_r2(filename, file_data):
-    # Replace with your Cloudflare R2 upload logic
-    print("from python", filename)
-    upload_file(filename, config["r2"]["bucket"])
-
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    compressed = sys.argv[2].lower() == 'true'
-    
-    file_data = sys.stdin.buffer.read()
+    try:
+        filename = sys.argv[1]
+        compressed = sys.argv[2].lower() == "true"
 
-    if compressed:
-        file_data = gzip.decompress(file_data)
+        print("before file read")
 
-    upload_to_r2(filename, file_data)
+        file_data = sys.stdin.buffer.read()
+
+        print("after file read")
+
+        if compressed and file_data:
+            file_data = gzip.decompress(file_data)
+
+        upload_to_r2(filename, file_data, config["r2"]["bucket"])
+    except Exception as e:
+        print(e)
+        raise e
