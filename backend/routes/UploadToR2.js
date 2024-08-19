b68257a5912ae832e8e6742415774f84f44b89e5
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-08-19 21:35:23 +0530

    Video processing

--- backend/routes/UploadToR2.js
+++ backend/routes/UploadToR2.js
@@ -3,8 +3,8 @@ const multer = require('multer')
 const { spawn } = require('child_process')
 const zlib = require('zlib')
 
-// const fs = require('fs')
-// const ffmpeg = require('fluent-ffmpeg')
+const fs = require('fs')
+const ffmpeg = require('fluent-ffmpeg')
 
 const {
   HTTP_OK,
@@ -575,6 +575,131 @@ router.post('/videos/process/', async (req, res) => {
     // fs.unlinkSync(`video.mp4`)
     return res.status(500).json({
       message: err || 'Failed to process video',
+    })
+  }
+})
+
+router.post('/video/process-ffmpeg', async (req, res) => {
+  console.log('ffmpeg processing request')
+  const { video_recording_id } = await req.json()
+
+  console.info('video_recording_id', video_recording_id)
+
+  if (!video_recording_id) {
+    return res.status(400).json({
+      message: 'video_recording_id is required',
+    })
+  }
+
+  let videoRecording = null
+  // const uploadId = null
+  let folder_name = null
+
+  try {
+    // console.log('foldername:', foldername)
+    // console.log('videos:', videos)
+    videoRecording = await VideoRecordings.findById(video_recording_id)
+
+    if (!videoRecording) {
+      return res.status(400).json({
+        message: 'Video recording not found',
+      })
+    }
+
+    console.info('videoRecording found', videoRecording._id)
+
+    folder_name = videoRecording.get('folder_name')
+
+    // get file from cloudflare
+
+    const file = await cloudflareGetFile(
+      'yoga-video-recordings',
+      `${folder_name}/${folder_name}_final.mp4`,
+      'video/mp4'
+    )
+
+    fs.writeFileSync(`input.mp4`, Buffer.from(file.buffer))
+
+    // const readableBuffer = Readable.from(file);
+    // const readableBuffer = new ReadableStream({
+    // 	start(controller) {
+    // 		controller.enqueue(file.buffer);
+    // 		controller.close();
+    // 	},
+    // });
+
+    // console.info("readableBuffer created");
+
+    // create a file
+    // fs.closeSync(fs.openSync(`${foldername}.mp4`, 'a'))
+    const fd = fs.openSync(`video.mp4`, 'a')
+
+    fs.closeSync(fd)
+
+    // const newBuf = await buffer();
+
+    console.info('file created')
+    // // process with ffmpeg
+    const command = ffmpeg(`input.mp4`)
+      .inputOption('-sn')
+      .inputOption('-strict', 'experimental')
+      .output(`video.mp4`)
+
+    command.run()
+
+    let status = -1
+    let maxWaitTime = 5 * 60
+
+    command.on('end', () => {
+      status = 1
+      console.info('ffmpeg completed')
+    })
+
+    command.on('error', (err) => {
+      console.error(err)
+      status = 0
+      console.error('ffmpeg error')
+    })
+
+    while (status === -1 && maxWaitTime > 0) {
+      console.log('waiting for ffmpeg to complete')
+      maxWaitTime -= 1
+      await sleep(1000)
+    }
+
+    if (status === -1 || maxWaitTime === 0) {
+      fs.unlinkSync(`video.mp4`)
+      return res.status(500).json({
+        message: 'Failed to process video',
+      })
+    }
+
+    // get buffer from file
+    const processedBuffer = fs.readFileSync(`video.mp4`)
+
+    console.log('processed buffer size:', processedBuffer.byteLength)
+
+    await cloudflareAddFileToBucket(
+      'yoga-video-recordings',
+      `${folder_name}/${folder_name}_final_ffmpeg.mp4`,
+      processedBuffer,
+      'video/mp4'
+    )
+
+    // delete the file
+    fs.unlinkSync(`video.mp4`)
+    fs.unlinkSync(`input.mp4`)
+
+    videoRecording.processing_status = 'PROCESSED_FFMPEG'
+    await videoRecording.save()
+
+    return res.status(200).json({
+      message: 'Video processed successfully',
+    })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      message: err,
     })
   }
 })
