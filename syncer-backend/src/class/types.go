688c0184ba419ab878b5974b8143cfbece207c3f
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-08-23 12:27:10 +0530

    Packages

--- syncer-backend/src/class/types.go
+++ syncer-backend/src/class/types.go
@@ -3,13 +3,13 @@ package class
 import "syncer-backend/src/events"
 
 type ClassModel struct {
-	ID            string                 `json:"id"`
-	Name          string                 `json:"name"`
-	Description   string                 `json:"description"`
-	InstituteID   string                 `json:"institute_id"`
-	TeacherID     string                 `json:"teacher_id"`
-	Students      []string               `json:"students"`
+	ID          string `json:"_id"`
+	Name        string `json:"class_name"`
+	Description string `json:"class_desc"`
+	// InstituteID   string                 `json:"institute_id"`
+	TeacherID string `json:"teacher_id"`
+	// Students      []string               `json:"students"`
 	ControlsQueue []events.ControlsEvent `json:"controls_queue"`
 	ActionsQueue  []events.QueueEvent    `json:"actions_queue"`
-	WatchHistory  []string               `json:"watch_history"`
+	// WatchHistory  []string               `json:"watch_history"`
 }
