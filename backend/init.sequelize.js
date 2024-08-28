5418bafd5cb4462b9ce7b4068abbda0ee9cc6f6e
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-08-28 22:12:11 +0530

    Auth fix

--- backend/init.sequelize.js
+++ backend/init.sequelize.js
@@ -1,18 +1,18 @@
-const dotenv = require("dotenv");
-const { Sequelize } = require("sequelize");
-const SQLite = require("sqlite3");
+const dotenv = require('dotenv')
+const { Sequelize } = require('sequelize')
+const SQLite = require('sqlite3')
 
-dotenv.config();
+dotenv.config()
 
 const DB_DATABASE = process.env.DB_DATABASE,
   DB_USERNAME = process.env.DB_USERNAME,
   DB_PASSWORD = process.env.DB_PASSWORD,
   DB_HOST = process.env.DB_HOST,
-  DB_PORT = process.env.DB_PORT;
+  DB_PORT = process.env.DB_PORT
 
 // console.log(DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 var sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: "postgres",
+  dialect: 'postgres',
   logging: false,
   host: DB_HOST,
   port: DB_PORT,
@@ -23,12 +23,12 @@ var sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     // },
   },
   benchmark: true,
-});
+})
 
 // Initializes and syncs the db
 function initializeSequelize() {
-  return sequelize.sync({ alter: true });
+  return sequelize.sync()
   // return sequelize.sync({ force: true });
 }
 
-module.exports = { sequelize, initializeSequelize };
+module.exports = { sequelize, initializeSequelize }
