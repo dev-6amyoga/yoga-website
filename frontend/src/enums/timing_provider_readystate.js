cf892e90d168eeca291a78343715069e4fdfa274
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-09-07 15:46:23 +0530

    Added readyState enums

---
+++ frontend/src/enums/timing_provider_readystate.js
@@ -0,0 +1,5 @@
+export const READYSTATE_INITIALIZED = "initialized",
+	READYSTATE_OPEN = "open",
+	READYSTATE_CONNECTING = "connecting",
+	READYSTATE_CLOSING = "closing",
+	READYSTATE_CLOSED = "closed";
