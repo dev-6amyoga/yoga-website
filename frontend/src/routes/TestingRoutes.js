39ed43e5d8bd29a57a42ac67c27223e5550b5dd5
Shreyas Kaundinya <shreyassk08@gmail.com>
2024-09-21 08:43:57 +0530

    Experimenting with shaka iframe

--- frontend/src/routes/TestingRoutes.js
+++ frontend/src/routes/TestingRoutes.js
@@ -8,59 +8,64 @@ import ManageClasses from "../pages/class-mode/teacher/ManageClasses";
 import StudentHome from "../pages/student/StudentHome";
 import TeacherPlaylistCreation from "../pages/teacher/TeacherPlaylistCreation";
 import DesignBoard from "../pages/testing/DesignBoard";
+import ShakaIframe from "../pages/testing/ShakaIframe";
 import ShakaVideo from "../pages/testing/ShakaVideo";
 import SocketPage from "../pages/testing/Socket";
 export const TestingRoutes = [
-  // {
-  //   path: "/testing/video",
-  //   element: <TestingVideo />,
-  // },
-  {
-    path: "/testing/shaka-video",
-    element: <ShakaVideo />,
-  },
-  {
-    path: "/testing/ai-student",
-    element: <StudentHome />,
-  },
-  {
-    path: "/testing/tp",
-    element: <TeacherPlaylistCreation />,
-  },
-  {
-    path: "/testing/design",
-    element: <DesignBoard />,
-  },
-  {
-    path: "/testing/auto-reg",
-    element: <AutomaticRegister />,
-  },
-  {
-    path: "/testing/socket",
-    element: <SocketPage />,
-  },
-  {
-    path: "/class/student/:class_id",
-    element: <ClassModeStudent />,
-  },
-  {
-    path: "/class/teacher/:class_id",
-    element: <ClassModeTeacher />,
-  },
-  {
-    path: "/class/teacher/:class_id/info",
-    element: <ClassInfoTeacher />,
-  },
-  {
-    path: "/class/student/:class_id/info",
-    element: <ClassInfoStudent />,
-  },
-  {
-    path: "/class/manage",
-    element: <ManageClasses />,
-  },
-  // {
-  //   path: "/testing/playbar",
-  //   element: <PlaybarPage />,
-  // },
+	// {
+	//   path: "/testing/video",
+	//   element: <TestingVideo />,
+	// },
+	{
+		path: "/testing/shaka-video",
+		element: <ShakaVideo />,
+	},
+	{
+		path: "/testing/shaka-iframe",
+		element: <ShakaIframe />,
+	},
+	{
+		path: "/testing/ai-student",
+		element: <StudentHome />,
+	},
+	{
+		path: "/testing/tp",
+		element: <TeacherPlaylistCreation />,
+	},
+	{
+		path: "/testing/design",
+		element: <DesignBoard />,
+	},
+	{
+		path: "/testing/auto-reg",
+		element: <AutomaticRegister />,
+	},
+	{
+		path: "/testing/socket",
+		element: <SocketPage />,
+	},
+	{
+		path: "/class/student/:class_id",
+		element: <ClassModeStudent />,
+	},
+	{
+		path: "/class/teacher/:class_id",
+		element: <ClassModeTeacher />,
+	},
+	{
+		path: "/class/teacher/:class_id/info",
+		element: <ClassInfoTeacher />,
+	},
+	{
+		path: "/class/student/:class_id/info",
+		element: <ClassInfoStudent />,
+	},
+	{
+		path: "/class/manage",
+		element: <ManageClasses />,
+	},
+	// {
+	//   path: "/testing/playbar",
+	//   element: <PlaybarPage />,
+	// },
 ];
