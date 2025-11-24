927768df716446d42e1bdc37154234e261a6c547
dev-6amyoga <dev.6amyoga@gmail.com>
2025-11-24 16:01:01 +0530

    plan ordering and naming

--- backend/routes/Plan.js
+++ backend/routes/Plan.js
@@ -192,7 +192,7 @@ router.get('/get-all-institute-plans', async (req, res) => {
     let plans = await Plan.findAll({
       where: {
         plan_user_type: PLAN_USER_TYPE_INSTITUTE,
-        name: { [Op.iLike]: '%HATHAYOGA%' }, // only plans whose name includes HATHAYOGA (case-insensitive)
+        name: { [Op.iLike]: '%YOGA%' }, // only plans whose name includes HATHAYOGA (case-insensitive)
       },
     })
 
