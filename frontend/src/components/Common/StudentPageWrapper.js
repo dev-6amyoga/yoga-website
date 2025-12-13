4f555822ce0c9b01703fb2b797cec29895e62898
dev-6amyoga <dev.6amyoga@gmail.com>
2025-12-13 23:03:39 +0530

    scroll fix

--- frontend/src/components/Common/StudentPageWrapper.js
+++ frontend/src/components/Common/StudentPageWrapper.js
@@ -2,16 +2,16 @@ import StudentNavMUI from "./StudentNavbar/StudentNavMUI";
 
 export default function StudentPageWrapper({ heading, children }) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <StudentNavMUI />
-      <div className="max-w-7xl mx-auto py-20 px-4 xl:px-0">
-        {heading ? (
-          <h1 className="pt-4 font-bold text-center">{heading}</h1>
-        ) : (
-          <></>
-        )}
-        <div className="mt-6">{children}</div>
-      </div>
-    </>
+
+      <main className="flex-1 overflow-y-auto">
+        <div className="max-w-7xl mx-auto pt-24 pb-10 px-4 xl:px-0">
+          {heading && <h1 className="pt-4 font-bold text-center">{heading}</h1>}
+
+          <div className="mt-6">{children}</div>
+        </div>
+      </main>
+    </div>
   );
 }
